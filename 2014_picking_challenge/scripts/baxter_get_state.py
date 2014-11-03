a83ebe50ea03ab37eced4f2715f3f57f7b4b2ad0
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-11-03 17:47:53 +0900

    Add scripts to move baxters arms in response to the position published as named baxter_pose_chatter

---
+++ 2014_picking_challenge/scripts/baxter_get_state.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import struct
+import rospy
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+from std_msgs.msg import Header
+
+import baxter_interface
+from baxter_core_msgs.srv import (
+    SolvePositionIK,
+    SolvePositionIKRequest,
+)
+
+
+def callback(pose):
+    # ns = "ExternalTools/" + 'right' + "/PositionKinematicsNode/IKService"
+    # iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+    # ikreq = SolvePositionIKRequest()
+    #
+    # ikreq.pose_stamp.append(pose)
+    # try:
+    #     rospy.wait_for_service(ns, 5.0)
+    #     resp = iksvc(ikreq)
+    # except (rospy.ServiceException, rospy.ROSException), e:
+    #     rospy.logerr("Service call failed: %s" % (e,))
+    #     return 1
+    #
+    # # Check if result valid, and type of seed ultimately used to get solution
+    # # convert rospy's string representation of uint8[]'s to int's
+    # resp_seeds = struct.unpack('<%dB' % len(resp.result_type),
+    #                            resp.result_type)
+    # if (resp_seeds[0] != resp.RESULT_INVALID):
+    #     seed_str = {
+    #                 ikreq.SEED_USER: 'User Provided Seed',
+    #                 ikreq.SEED_CURRENT: 'Current Joint Angles',
+    #                 ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
+    #                }.get(resp_seeds[0], 'None')
+    #     print("SUCCESS - Valid Joint Solution Found from Seed Type: %s" %
+    #           (seed_str,))
+    #     # Format solution into Limb API-compatible dictionary
+    #     limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
+    #     print "\nIK Joint Solution:\n", limb_joints
+    #     print "------------------"
+    #     print "Response Message:\n", resp
+    # else:
+    #     print("INVALID POSE - No Valid Joint Solution Found.")
+
+    log = rospy.get_name() + ": I heard {}".format(pose)
+    rospy.loginfo(log)
+
+    right = baxter_interface.Limb('right')
+    rj = right.joint_names()
+    # for i, joint_name in enumerate(rj):
+    #     current_position = right.joint_angle(joint_name)
+    #     print "*", current_position, resp.joints[0].position[i]
+
+        # joint_command = {joint_name: resp.joints[0].position[i]}
+        # right.set_joint_positions(joint_command)
+        # rospy.sleep(0.1)
+    print right.endpoint_pose()
+    rospy.sleep(3)
+
+
+
+
+def listener():
+    rospy.init_node('baxter_pose_listener', anonymous=True)
+
+    # initialize arm position
+    # right = baxter_interface.Limb('right')
+    # right.move_to_neutral()
+    # log = rospy.get_name() + ": I heard {}".format(pose)
+    # rospy.loginfo(log)
+
+    right = baxter_interface.Limb('right')
+    print right.endpoint_pose()
+    rospy.sleep(3)
+
+
+if __name__ == '__main__':
+    listener()
---
+++ 2014_picking_challenge/scripts/baxter_pose_talker.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import rospy
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+from std_msgs.msg import Header
+
+
+def talker():
+    rospy.init_node('baxter_pose_talker')
+    pub = rospy.Publisher('baxter_pose_chatter', PoseStamped)
+
+    hdr = Header(stamp=rospy.Time.now(), frame_id='base')
+    right_pose = PoseStamped(
+        header=hdr,
+        pose=Pose(
+            position=Point(
+                x=0.9543684236857687,
+                y=-0.2900063802027193,
+                z=0.40121855877314805,
+            ),
+            orientation=Quaternion(
+                x=0.05532824006190543,
+                y=0.8002393257489635,
+                z=0.09079879412889569,
+                w=0.5901791137961719,
+            ),
+        ),
+    )
+
+    while not rospy.is_shutdown():
+        log = "hello, baxter. I'm talking right_pose: {}".format(right_pose)
+        rospy.loginfo(log)
+        pub.publish(right_pose)
+        rospy.sleep(1.0)
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
