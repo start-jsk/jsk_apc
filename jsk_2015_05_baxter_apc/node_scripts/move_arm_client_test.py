6d074103ee896c81d5e88a743c5d79f2a00f90c7
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-11-19 18:58:45 +0900

    Complete right arm moving script

--- jsk_2015_05_baxter_apc/node_scripts/move_arm_client_test.py
+++ jsk_2015_05_baxter_apc/node_scripts/move_arm_client_test.py
@@ -18,32 +18,27 @@ from jsk_2014_picking_challenge.srv import MoveArm
 
 
 def move_arm_client(pose):
-    rospy.wait_for_service('move_arm')
+    rospy.wait_for_service('semi/move_arm')
     try:
-        move_arm = rospy.ServiceProxy('move_arm', MoveArm)
+        move_arm = rospy.ServiceProxy('semi/move_arm', MoveArm)
         resp = move_arm(pose)
         return resp.succeeded
-    # try:
-    #     add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
-    #     resp1 = add_two_ints(x, y)
-    #     return resp1.sum
     except rospy.ServiceException, e:
         print "Service call failed: %s" % e
 
-# def usage():
-#     return "%s [x y]" % sys.argv[0]
 
 if __name__ == '__main__':
-    # if len(sys.argv) == 3:
-    #     x = int(sys.argv[1])
-    #     y = int(sys.argv[2])
-    # else:
-    #     print usage()
-    #     sys.exit(1)
     rospy.init_node('move_arm_client')
     hdr = Header(stamp=rospy.Time.now(), frame_id='base')
-    position = Point(x=0.7241942937871891, y=-0.39845057139098894, z=0.5456768943508293)
-    orientation = Quaternion(x=0.0017747791414926728, y=0.9587746912729604, z=0.006296746310126851, w=0.2840920493772123)
+    position = Point(
+            x=0.7241942937871891,
+            y=-0.39845057139098894,
+            z=0.5456768943508293)
+    orientation = Quaternion(
+            x=0.0017747791414926728,
+            y=0.9587746912729604,
+            z=0.006296746310126851,
+            w=0.2840920493772123)
     pose = Pose(position=position, orientation=orientation)
     right_pose = PoseStamped(header=hdr, pose=pose)
 
