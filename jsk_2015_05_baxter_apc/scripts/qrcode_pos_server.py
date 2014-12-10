8534d9019b30d65c872647c8822145f7a48fd0ab
iori <iori@iori-Macubuntu.(none)>
2014-12-10 22:19:17 +0900

    modified angles

--- jsk_2015_05_baxter_apc/scripts/qrcode_pos_server.py
+++ jsk_2015_05_baxter_apc/scripts/qrcode_pos_server.py
@@ -8,7 +8,6 @@ import rospy
 
 import baxter_interface
 
-
 from zbar_ros.msg import Marker
 from jsk_2014_picking_challenge.srv import *
 from jsk_2014_picking_challenge.msg import *
@@ -23,11 +22,11 @@ from geometry_msgs.msg import (
 
 baxter_arm_angles = [
     dict(zip(['right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2', 'right_e0', 'right_e1'],
-             [0.35856800875854494, -0.23546605067138673, 0.11581554935302735, 0.14802914586181642, 2.8907867914672853, 0.45482530308837893, 1.0611312088073732]
+             [1.0688011127380372, 1.0492428577148438, 3.0587576875488285, 0.24697090656738283, 3.0595246779418948, -3.046869336456299, 1.0580632472351075]
          )),
     dict(zip(['right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2', 'right_e0', 'right_e1'],
-[0.7873156384826661, -0.681470964239502, -1.089126358154297, -0.3819612157470703, 1.742602173046875, -0.4816699668457032, 0.5506991022216797]
-)),
+             [0.872068076916504, 1.0312185834777832, 3.0587576875488285, 0.6906748489562988, 3.0587576875488285, -3.0457188508666992, 1.2168302585998536]
+         ))
 ]
 
 marker_id = ""
@@ -42,16 +41,15 @@ def handle_qrcode_pos(req):
     right = baxter_interface.Limb('right')
     rj = right.joint_names()
 
-    print("joint_names()-->")
-    print(rj)
+    # print("joint_names()-->")
+    # print(rj)
     # rospy.Rate(1)
     for angle in baxter_arm_angles:
-        print('right joint_angles=>')
-        print(right.joint_angles())
+        # print('right joint_angles=>')
+        # print(right.joint_angles())
         right.move_to_joint_positions(angle)
 
         global marker_id
-        # rospy.sleep()
         hdr = Header(stamp=rospy.Time.now(), frame_id=marker_id[:5])
         lim = baxter_interface.Limb('right')
         pprint.pprint(lim.endpoint_pose())
@@ -66,7 +64,7 @@ def handle_qrcode_pos(req):
 
 def qrcode_pos_server():
     rospy.init_node("qrcode_pos_server")
-    s = rospy.Service('qrcode_pos', QrStampsrv, handle_qrcode_pos)
+    s = rospy.Service('semi/qrcode_pos', QrStampsrv, handle_qrcode_pos)
     print("Ready to return qrcode_pos")
     rospy.Subscriber('markers', Marker, callback)
     rospy.spin()
