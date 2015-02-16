27714371fb0e6f42fda56a00ade1430d880e3713
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-02-16 21:56:18 +0900

    add listener for moving arm to target picking region

---
+++ jsk_2015_05_baxter_apc/euslisp/test_move2target.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+rostopic pub /semi/move_larm2target std_msgs/String 'f' -1
+sleep 7
+rostopic pub /semi/move_larm2target std_msgs/String 'e' -1
+sleep 7
+rostopic pub /semi/move_larm2target std_msgs/String 'd' -1
+sleep 7
+rostopic pub /semi/move_larm2target std_msgs/String 'c' -1
+sleep 7
+rostopic pub /semi/move_larm2target std_msgs/String 'b' -1
+sleep 7
+rostopic pub /semi/move_larm2target std_msgs/String 'a' -1
+sleep 7
+rostopic pub /semi/move_rarm2target std_msgs/String 'g' -1
+sleep 7
+rostopic pub /semi/move_rarm2target std_msgs/String 'h' -1
+sleep 7
+rostopic pub /semi/move_rarm2target std_msgs/String 'i' -1
+sleep 7
+rostopic pub /semi/move_rarm2target std_msgs/String 'j' -1
+sleep 7
+rostopic pub /semi/move_rarm2target std_msgs/String 'k' -1
+sleep 7
+rostopic pub /semi/move_rarm2target std_msgs/String 'l' -1
+sleep 7
+
