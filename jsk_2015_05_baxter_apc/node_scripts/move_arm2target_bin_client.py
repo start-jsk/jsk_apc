bec90050294b3a823a7704a6eb0ab6a0c93e7ae2
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-02-20 21:50:10 +0900

    cmdline arguments to order the target bin

--- jsk_2015_05_baxter_apc/node_scripts/move_arm2target_bin_client.py
+++ jsk_2015_05_baxter_apc/node_scripts/move_arm2target_bin_client.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #
 import os
+import argparse
 
 import rospy
 import actionlib
-# from actionlib_tutorials.msg import FibonacciAction, FibonacciGoal
 from jsk_2014_picking_challenge.msg import (
     MoveArm2TargetBinAction,
     MoveArm2TargetBinGoal,
@@ -12,22 +12,33 @@ from jsk_2014_picking_challenge.msg import (
 
 
 def main():
+    # cmdline arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('order', help='bin name for arm to move')
+    args = parser.parse_args(rospy.myargv()[1:])
+
+    # initialize
     rospy.init_node('move_arm2target_bin_client')
     client = actionlib.SimpleActionClient('move_arm2target_bin', MoveArm2TargetBinAction)
+
+    # wait for server to boot
     print("{0} wait_for_server".format(os.getpid()))
     client.wait_for_server()
 
+    # request goal for arm to move
     goal = MoveArm2TargetBinGoal()
-    goal.order = 'a'
-    print "Requesting fibonacci {0}".format(goal.order)
-
+    goal.order = args.order
+    print "Requesting move for bin {0}".format(goal.order)
     client.send_goal(goal)
 
+    # wait for result
     print("{0} wait_for_result".format(os.getpid()))
-    client.wait_for_result(rospy.Duration.from_sec(10.0))
+    client.wait_for_result(rospy.Duration.from_sec(20.0))
 
+    # get result
     result = client.get_result()
-    print("Resulting fibonacci {0}".format(result.sequence))
+    if result:
+        print("Resulting move for bin {0}".format(result.sequence))
 
 
 if __name__ == '__main__':
