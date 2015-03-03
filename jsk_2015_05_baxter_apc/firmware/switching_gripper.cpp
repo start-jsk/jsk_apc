aacd2e0136a34576ff797d4797a419588428a6a1
iori <ab.ioryz@gmail.com>
2015-03-03 17:47:32 +0900

    add grippe-control. Please load "gripper-control.l" file.

--- jsk_2015_05_baxter_apc/firmware/switching_gripper.cpp
+++ jsk_2015_05_baxter_apc/firmware/switching_gripper.cpp
@@ -5,28 +5,28 @@
 #include <ros.h>
 #include <std_msgs/Empty.h>
 #include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
 const int PIN = 13;
 
-ros::NodeHandle  nh;
+ros::NodeHandle nh;
 
 // following code did not work well.
 // $rostopic pub /on_off_gripper std_msgs/String {data: "ON"}
 
-// void messageCb( const std_msgs::String& toggle_msg){
-//     if(toggle_msg.data == "ON" || toggle_msg.data == "on"){
-//         digitalWrite(PIN, HIGH);
-//     } else if(toggle_msg.data == "OFF" || toggle_msg.data == "off"){
-//         digitalWrite(PIN, LOW);
-//     }
-//     //digitalWrite(PIN, HIGH-digitalRead(PIN));   // Switch SSR
-// }
-
-void messageCb(const std_msgs::Empty& toggle_msg){
-    digitalWrite(PIN, HIGH-digitalRead(PIN));   // Switch SSR
+void messageCb( const std_msgs::Bool& toggle_msg){
+    if(toggle_msg.data){
+        digitalWrite(PIN, HIGH);
+    } else {
+        digitalWrite(PIN, LOW);
+    }
 }
 
-//ros::Subscriber<std_msgs::String> sub("on_off_gripper", &messageCb);
-ros::Subscriber<std_msgs::Empty> sub("on_off_gripper", &messageCb);
+// void messageCb(const std_msgs::Empty& toggle_msg){
+//     digitalWrite(PIN, HIGH-digitalRead(PIN));   // Switch SSR
+// }
+
+ros::Subscriber<std_msgs::Bool> sub("on_off_gripper", &messageCb);
+//ros::Subscriber<std_msgs::Empty> sub("on_off_gripper", &messageCb);
 
 // publish state <on/off>
 std_msgs::String str_msg;
