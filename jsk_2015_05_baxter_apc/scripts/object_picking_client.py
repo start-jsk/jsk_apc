083f1077a035f9b9f2f3096257375b79c4dae6c0
iori <ab.ioryz@gmail.com>
2015-03-04 11:35:58 +0900

    modified object picking to add cleaner on/off function.

--- jsk_2015_05_baxter_apc/scripts/object_picking_client.py
+++ jsk_2015_05_baxter_apc/scripts/object_picking_client.py
@@ -9,13 +9,13 @@ from jsk_2014_picking_challenge.msg import *
 
 def main():
     rospy.init_node("object_picking_client")
-    # client = actionlib.SimpleActionClient("object_picking", MoveArm2TargetBinAction)
     client = actionlib.SimpleActionClient("object_picking", ObjectPickingAction)
     print("{} wait_for_server".format(os.getpid()))
     client.wait_for_server()
 
-    goal = MoveArm2TargetBinGoal()
-    goal.limb = 'left'
+    goal = ObjectPickingGoal()
+    goal.limb = 'larm'
+    goal.state = False
     print("Requesting ...")
 
     client.send_goal(goal)
@@ -24,8 +24,7 @@ def main():
     client.wait_for_server(rospy.Duration.from_sec(10.0))
 
     result = client.get_result()
-    print(result)
-    # print("Resulting {}".format(result.sequence))
+    print result
 
 if __name__ == "__main__":
     main()
