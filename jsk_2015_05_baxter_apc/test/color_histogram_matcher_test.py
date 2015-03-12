136515a63b2c3651f98ed244682f0ae2cbf9ba52
iori <ab.ioryz@gmail.com>
2015-03-12 19:07:44 +0900

    add modified color match's coefficient

--- jsk_2015_05_baxter_apc/test/color_histogram_matcher_test.py
+++ jsk_2015_05_baxter_apc/test/color_histogram_matcher_test.py
@@ -11,13 +11,14 @@ def color_histogram_matcher_client(object_names):
     try:
         probs = rospy.ServiceProxy('/semi/color_histogram_matcher', ObjectMatch)
         resp = probs(object_names)
-        print resp
-        return resp.probabilities
+        print(object_names)
+        return resp
     except rospy.ServiceException, e:
         rospy.loginfo("Service call faield. {}".format(e))
 
 def main():
-    print(color_histogram_matcher_client(['champion_copper_plus_spark_plug', 'cheezit_big_original', 'crayola_64_ct']))
+    print(color_histogram_matcher_client(['champion_copper_plus_spark_plug', 'crayola_64_ct']))
+    print(color_histogram_matcher_client(['elmers_washable_no_run_school_glue', 'cheezit_big_original', 'expo_dry_erase_board_eraser']))
 
 if __name__ == "__main__":
     main()
