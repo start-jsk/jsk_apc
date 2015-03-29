Fix wrong order probability response for the request

jsk_2014_picking_challenge/scripts/color_histogram_matcher.py
@@ -43,7 +43,8 @@ class ColorHistogramMatcher(object):
         """Handler of service request"""
         rospy.loginfo("handl_colohhist_matcher")
         self.load_target_histograms(req.objects)
-        return ObjectMatchResponse(probabilities=self.get_probabilities())
+        probs = self.get_probabilities(req.objects)
+        return ObjectMatchResponse(probabilities=probs)
 
     def load_target_histograms(self, object_names):
         """Load extracted color histogram features of objects"""
@@ -67,29 +68,32 @@ class ColorHistogramMatcher(object):
             return (1. + cv2.compareHist(query_hist, target_hist,
                 cv2.cv.CV_COMP_CORREL)) / 2.;
 
-    def get_probabilities(self):
-        """Get probabilities of color matching"""
+    def get_probabilities(self, object_names):
+        """Get probabilities of color matching
+        (the order of object_names and probabilities is same)
+        """
         rospy.loginfo("get probabilities")
 
         query_histogram = self.query_histogram
         targets_histograms = self.target_histograms
         obj_coefs = []
-        for obj_name, target_histograms in targets_histograms.iteritems():
+        for obj_name in object_names:
+            target_histograms = targets_histograms[obj_name]
             # loop for RGB color &
             # compute max coefficient about each histograms
             coefs = []
             for color in query_histogram.keys():
                 q_hist = query_histogram[color]
                 t_hists = target_histograms[color]
-                coefs.append(sum(self.coefficient(q_hist, t_hist)
+                coefs.append(max(self.coefficient(q_hist, t_hist)
                                  for t_hist in t_hists))
             obj_coefs.append(sum(coefs))
         obj_coefs = np.array(obj_coefs)
         # change coefficient array to probability array
-        if obj_coefs.sum() == 0:
-            return obj_coefs
-        else:
+        try:
             return obj_coefs / obj_coefs.sum()
+        except ZeroDivisionError:
+            return obj_coefs
 
     def cb_histogram_red(self, msg):
         """Get input red histogram"""