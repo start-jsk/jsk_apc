9377e93ba007d9527df81454584d8e84549dcce0
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-05-01 21:10:28 +0900

    Separate bof extractor and histogram creator

---
+++ jsk_2015_05_baxter_apc/scripts/extract_bof.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import os
+import sys
+import gzip
+import argparse
+import cPickle as pickle
+
+import numpy as np
+
+from matcher_common import get_object_list, load_siftdata
+
+
+def get_sift_descriptors(n_imgs=None, data_dir=None):
+    objects = get_object_list()
+    obj_descs = []
+    for obj in objects:
+        descs = load_siftdata(obj_name=obj,
+                              return_pos=False,
+                              data_dir=data_dir)
+        if descs is None:
+            continue
+        if n_imgs is None:
+            n_imgs = len(descs)
+        p = np.random.randint(0, len(descs), size=n_imgs)
+        descs = np.array(map(lambda x: x.astype('float16'), descs))
+        obj_descs.append((obj, descs[p]))
+    return obj_descs
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('siftdata_dir')
+    parser.add_argument('n_imgs')
+    args = parser.parse_args(sys.argv[1:])
+
+    print('getting descriptors...')
+    sift_descs = get_sift_descriptors(n_imgs=args.n_imgs,
+                                      data_dir=args.siftdata_dir)
+    _, descs = zip(*sift_descs)
+    X = []
+    for d in descs:
+        xi = np.vstack(map(lambda x: x.reshape((-1, 128)), d))
+        X.append(xi)
+    X = np.vstack(X)
+    np.random.shuffle(X)
+    rospy.loginfo('X.shape: {}'.format(X.shape))
+
+    print('fitting bag of features...')
+    bof = BagOfFeatures()
+    bof.fit(X)
+    bof.save_bof(path='bof.pkl.gz')
+
+
+if __name__ == '__main__':
+    main()
