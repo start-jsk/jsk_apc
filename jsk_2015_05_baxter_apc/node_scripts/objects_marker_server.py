753b0aa7d6c5156258044a1a18ba385437124693
Yuto Inagaki <yuto.inagaki@gmail.com>
2015-05-19 00:01:31 +0900

    Update objects marker server

--- jsk_2015_05_baxter_apc/node_scripts/objects_marker_server.py
+++ jsk_2015_05_baxter_apc/node_scripts/objects_marker_server.py
@@ -39,7 +39,7 @@ from geometry_msgs.msg import *
 from visualization_msgs.msg import *
 from tf import transformations
 from tf import TransformListener
-from jsk_2014_picking_challenge.msg import SetObjectPositionArray
+from jsk_2014_picking_challenge.msg import SetObjectPositionArray, WorkOrderArray
 import rospkg
 import yaml
 server = None
@@ -55,14 +55,15 @@ def frameCallback( msg ):
     if objects_name_list:
         for object_name in objects_name_list:
             int_marker = server.get(object_name)
-            cur_pose = int_marker.pose
-            quat = transformations.quaternion_about_axis(2 * 3.14159286 * (1.0 / 1000), (0,0,1))
-            quat = transformations.quaternion_multiply([cur_pose.orientation.x, cur_pose.orientation.y, cur_pose.orientation.z, cur_pose.orientation.w], quat)
-            cur_pose.orientation.x = quat[0]
-            cur_pose.orientation.y = quat[1]
-            cur_pose.orientation.z = quat[2]
-            cur_pose.orientation.w = quat[3]
-            server.setPose(int_marker.name, cur_pose);
+            if int_marker:
+                cur_pose = int_marker.pose
+                quat = transformations.quaternion_about_axis(2 * 3.14159286 * (1.0 / 1000), (0,0,1))
+                quat = transformations.quaternion_multiply([cur_pose.orientation.x, cur_pose.orientation.y, cur_pose.orientation.z, cur_pose.orientation.w], quat)
+                cur_pose.orientation.x = quat[0]
+                cur_pose.orientation.y = quat[1]
+                cur_pose.orientation.z = quat[2]
+                cur_pose.orientation.w = quat[3]
+                server.setPose(int_marker.name, cur_pose);
         server.applyChanges()
 
 def processFeedback( feedback ):
@@ -79,6 +80,25 @@ def makeTargetObject( msg, object_name ):
     marker.mesh_use_embedded_materials = True
     return marker
 
+def makeBoardObject( msg , object_name):
+    marker = Marker()
+    marker.type = Marker.CUBE
+    marker.scale.x = 1
+    marker.scale.y = 5
+    marker.scale.z = 0.1
+    if object_name == "left_board":
+        marker.color.r = 1
+        marker.color.g = 0.2
+        marker.color.b = 0.2
+        marker.color.a = 0.5
+    else:
+        marker.color.r = 0.2
+        marker.color.g = 0.2
+        marker.color.b = 1
+        marker.color.a = 0.5
+        
+    return marker
+
 def makeTargetObjectControl( msg, object_name ):
     control =  InteractiveMarkerControl()
     control.always_visible = True
@@ -86,6 +106,28 @@ def makeTargetObjectControl( msg, object_name ):
     msg.controls.append( control )
     return control
 
+def makeBoardObjectControl( msg, object_name ):
+    control =  InteractiveMarkerControl()
+    control.always_visible = True
+    control.markers.append( makeBoardObject(msg, object_name) )
+    msg.controls.append( control )
+    return control
+
+def makeInteractiveBoardObject( object_name, position, quaternion):
+    global base_frame_id
+    int_marker = InteractiveMarker()
+    int_marker.header.frame_id = base_frame_id
+    int_marker.pose.position = position
+    int_marker.pose.orientation = quaternion
+    int_marker.scale = 1
+
+    makeBoardObjectControl(int_marker, object_name)
+    int_marker.controls[0].interaction_mode = InteractiveMarkerControl.NONE
+
+    int_marker.name  = object_name 
+    int_marker.description = object_name
+    server.insert(int_marker, processFeedback)
+    
 
 def make6DofMarker( object_name, position, quaternion ):
     global base_frame_id
@@ -105,17 +147,37 @@ def make6DofMarker( object_name, position, quaternion ):
 def setObjectPoses(msg):
     for target_object in msg.objects:
         int_marker = server.get(target_object.object_name)
-        tl = TransformListener()
-        transed_point = tl.transformPoint(base_frame_id, target_object.position)
-        cur_pose = int_marker.pose
-        cur_pose.position = transed_point.point
-        server.setPose(int_marker.name, cur_pose);        
+        if int_marker:
+            tl = TransformListener()
+            transed_point = tl.transformPoint(base_frame_id, target_object.position)
+            cur_pose = int_marker.pose
+            cur_pose.position = transed_point.point
+            server.setPose(int_marker.name, cur_pose);        
     server.applyChanges()
+
+def setWithWorkOrder(msg, callback_args):
+    arm = callback_args["arm"]
+    counter = 0
+    for target_object in msg.array:
+        print target_object
+        int_marker = server.get(target_object.object)
+        if int_marker:
+            tl = TransformListener()
+            cur_pose = Pose()
+            cur_pose.position.x = -1 if arm == "left" else 1
+            cur_pose.position.y = 0.5 + counter * 0.5
+            cur_pose.position.z = 0
+            cur_pose.orientation = int_marker.pose.orientation
+            server.setPose(int_marker.name, cur_pose);
+    server.applyChanges()    
 
 if __name__=="__main__":
     rospy.init_node("target_object_marker_server")
     
     rospy.Subscriber('~set_pose', SetObjectPositionArray, setObjectPoses)
+
+    rospy.Subscriber('/left/work_order_list', WorkOrderArray, setWithWorkOrder, {"arm":"left"})
+    rospy.Subscriber('/right/work_order_list', WorkOrderArray, setWithWorkOrder, {"arm":"right"})
 
     rospy.Timer(rospy.Duration(0.01), frameCallback)
     server = InteractiveMarkerServer("target_object_marker_server")    
@@ -128,6 +190,13 @@ if __name__=="__main__":
         quat = transformations.quaternion_about_axis(2 * 3.14159286 * (i * 1.0 / len(objects_name_list)), (0,0,1))
         quaternion = Quaternion(quat[0], quat[1], quat[2], quat[3])
         make6DofMarker( object_name, position, quaternion )
+
+    position = Point( -2.8, -0.8, 0)
+    quat = transformations.quaternion_about_axis(3.14159286/2, (0,0,1))
+    quaternion = Quaternion(quat[0], quat[1], quat[2], quat[3])
+    makeInteractiveBoardObject("left_board", position, quaternion)
+    position = Point( -2.8, 0.8,  0)
+    makeInteractiveBoardObject("right_board", position, quaternion)
 
     server.applyChanges()
     rospy.spin()
