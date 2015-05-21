ab27a104c99b66a4e00d75f119f338755788cbe2
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-05-21 13:44:52 +0900

    [euslisp/robot-main.l][launch/main.launch][scripts/setup_params.py] Start with user input

--- jsk_2015_05_baxter_apc/scripts/setup_params.py
+++ jsk_2015_05_baxter_apc/scripts/setup_params.py
@@ -5,6 +5,7 @@
 import rospy
 
 from work_order import get_sorted_work_order
+from rqt_yn_btn.srv import YesNo
 
 
 def main():
@@ -23,15 +24,31 @@ def main():
         elif first_orders['left'] == '' and first_orders['right'] == '':
             break
 
-    rospy.set_param('left_process/target', first_orders['left'])
-    rospy.set_param('right_process/target', first_orders['right'])
-    rospy.set_param('/left_process/state', 'pick_object')
-    rospy.set_param('/right_process/state', 'pick_object')
+    rospy.set_param('/left_process/target', first_orders['left'])
+    rospy.set_param('/right_process/target', first_orders['right'])
+    rospy.set_param('/left_process/state', 'wait_for_user_input')
+    rospy.set_param('/right_process/state', 'wait_for_user_input')
 
     rospy.set_param('/total_score', 0)
+
+    # wait for rqt_yn_btn
+    wait_for_rqt_yn_btn = True
+    while wait_for_rqt_yn_btn:
+        try:
+            rospy.wait_for_service('/rqt_yn_btn', timeout=10)
+            wait_for_rqt_yn_btn = False
+        except rospy.ROSException, e:
+            rospy.logerr('timeout for /rqt_yn_btn: {0}'.format(e))
+    # get user input using rqt_yn_btn
+    cl_yn = rospy.ServiceProxy('/rqt_yn_btn', YesNo)
+    start_main = False
+    while start_main is False:
+        res = cl_yn()
+        start_main = res.yes
+    rospy.set_param('/left_process/state', 'pick_object')
+    rospy.set_param('/right_process/state', 'pick_object')
 
 
 if __name__ == '__main__':
     rospy.init_node('setup_target')
     main()
-    quit()
