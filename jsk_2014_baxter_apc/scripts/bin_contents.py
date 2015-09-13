161359763bc93fa783235e8706209c4cae798ead
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-14 04:32:08 +0900

    Move BinContents

--- jsk_2014_baxter_apc/scripts/bin_contents.py
+++ jsk_2014_baxter_apc/scripts/bin_contents.py
@@ -3,7 +3,7 @@
 import json
 
 import rospy
-from jsk_2014_baxter_apc.msg import BinContents, BinContentsArray
+from jsk_2014_apc.msg import BinContents, BinContentsArray
 
 
 def get_bin_contents(json_file):
