a28022042ae9f50067004cccc1afb7a6f8f2c617
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-01-29 22:44:48 +0900

    Use RosPack for data file

--- jsk_apc2015_common/src/jsk_apc2015_common/__init__.py
+++ jsk_apc2015_common/src/jsk_apc2015_common/__init__.py
@@ -1,1 +1,15 @@
-from jsk_apc2015_common.data import get_object_list
+import os
+import yaml
+
+import rospkg
+
+PKG = 'jsk_apc2015_common'
+rp = rospkg.RosPack()
+
+
+def get_object_list():
+    pkg_path = rp.get_path(PKG)
+    yaml_file = os.path.join(pkg_dir, 'data/object_list.yml')
+    with open(yaml_file) as f:
+        objects = yaml.load(f)
+    return objects
