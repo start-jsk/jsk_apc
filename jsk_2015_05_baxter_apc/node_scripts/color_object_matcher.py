34ce7c39086ec0428bdea0d2cd8d3b919043b019
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-02-03 00:34:17 +0900

    Update color_histogram object recognition for multi regions

--- jsk_2015_05_baxter_apc/node_scripts/color_object_matcher.py
+++ jsk_2015_05_baxter_apc/node_scripts/color_object_matcher.py
@@ -5,58 +5,78 @@
 this script will test classification by the color_histogram
 """
 
-import rospy
-from sensor_msgs.msg import Image
-from jsk_recognition_msgs.msg import ColorHistogram
-from jsk_2015_05_baxter_apc.msg import ObjectRecognition
-import jsk_apc2015_common
-from common import ObjectMatcher
-from color_histogram_features import ColorHistogramFeatures
-import cv_bridge
-
 import numpy as np
 
+import cv_bridge
+import jsk_apc2015_common
+from jsk_recognition_msgs.msg import ClassificationResult
+from jsk_topic_tools import ConnectionBasedTransport
 
-class ColorObjectMatcher(ObjectMatcher):
+import jsk_recognition_utils
+import message_filters
+import rospy
+from sensor_msgs.msg import Image
+
+from color_histogram_features import ColorHistogramFeatures
+
+
+class ColorObjectMatcher(ConnectionBasedTransport):
 
     def __init__(self):
-        super(ColorObjectMatcher, self).__init__('/color_object_matcher')
-        self._pub_recog = rospy.Publisher('~output', ObjectRecognition,
-                                          queue_size=1)
-        self._pub_debug = rospy.Publisher(
-            '~debug', ColorHistogram, queue_size=1)
-        self.query_image = None
+        super(ColorObjectMatcher, self).__init__()
+        self._pub = self.advertise('~output', ClassificationResult,
+                                   queue_size=1)
         self.estimator = ColorHistogramFeatures()
         self.estimator.load_data()
-        rospy.Subscriber('~input', Image, self._predict)
 
-    def _predict(self, msg):
+    def subscribe(self):
+        self.sub_img = message_filters.Subscriber('~input', Image)
+        self.sub_label = message_filters.Subscriber('~input/label', Image)
+        queue_size = rospy.get_param('~queue_size', 100)
+        if rospy.get_param('~approximate_sync', False):
+            sync = message_filters.ApproximateTimeSynchronizer(
+                [self.sub_img, self.sub_label], queue_size=queue_size,
+                slop=0.1)
+        else:
+            sync = message_filters.TimeSynchronizer(
+                [self.sub_img, self.sub_label], queue_size=queue_size)
+        sync.registerCallback(self._predict)
+
+    def unsubscribe(self):
+        self.sub_img.sub.unregister()
+        self.sub_label.sub.unregister()
+
+    def _predict(self, img_msg, label_msg):
         # convert image
         bridge = cv_bridge.CvBridge()
-        input_image = bridge.imgmsg_to_cv2(msg, 'rgb8')
-        # compute histogram
-        hist = self.estimator.color_hist(input_image)
-        self._pub_debug.publish(
-            ColorHistogram(header=query_image.header, histogram=hist))
+        input_image = bridge.imgmsg_to_cv2(img_msg, 'rgb8')
+        input_label = bridge.imgmsg_to_cv2(label_msg)
         # predict
-        proba = self.estimator.predict(input_image)[0]
-        objects = jsk_apc2015_common.get_object_list()
-        matched_idx = np.argmax(proba)
+        region_imgs = []
+        for l in np.unique(input_label):
+            if l == 0:  # bg_label
+                continue
+            mask = (input_label == l)
+            region = jsk_recognition_utils.bounding_rect_of_mask(
+                input_image, mask)
+            region_imgs.append(region)
+        y_proba = self.estimator.predict(region_imgs)
+        target_names = np.array(jsk_apc2015_common.get_object_list())
+        y_pred = np.argmax(y_proba, axis=-1)
+        label_proba = [p[i] for p, i in zip(y_proba, y_pred)]
         # prepare message
-        res = ObjectRecognition()
-        res.header = msg.header
-        res.matched = objects[matched_idx]
-        res.probability = proba[matched_idx]
-        res.candidates = objects
-        res.probabilities = proba
-        return res
-
-
-def main():
-    rospy.init_node('color_object_matcher')
-    matcher = ColorObjectMatcher()
-    rospy.spin()
+        res = ClassificationResult()
+        res.header = img_msg.header
+        res.labels = y_pred
+        res.label_names = target_names[y_pred]
+        res.label_proba = label_proba
+        res.probabilities = y_proba.reshape(-1)
+        res.classifier = '<jsk_2015_05_baxter_apc.ColorHistogramFeatures>'
+        res.target_names = target_names
+        self._pub.publish(res)
 
 
 if __name__ == "__main__":
-    main()
+    rospy.init_node('color_object_matcher')
+    ColorObjectMatcher()
+    rospy.spin()
