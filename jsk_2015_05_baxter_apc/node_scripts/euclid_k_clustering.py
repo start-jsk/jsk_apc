593cb6b4390e4c5a9da0d52e87accd4a3123dc44
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-02-25 20:22:05 +0900

    EuclidKClustering with number of objects in bin

---
+++ jsk_2015_05_baxter_apc/node_scripts/euclid_k_clustering.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import threading
+
+import dynamic_reconfigure.client
+import rospy
+from jsk_recognition_msgs.msg import Int32Stamped
+
+
+expected_n_cluster = None
+lock = threading.Lock()
+
+
+def cb_kcluster(msg):
+    global expected_n_cluster
+    with lock:
+        expected_n_cluster = msg.data
+
+
+def cb(msg):
+    global expected_n_cluster
+    with lock:
+        if expected_n_cluster is None:
+            return
+        cfg = client.get_configuration(timeout=None)
+        tol_orig = cfg['tolerance']
+        delta = tol_orig * reconfig_eps
+        if msg.data == expected_n_cluster:
+            print('Expected/Actual n_cluster: {0}, Tolerance: {1}'
+                .format(msg.data, tol_orig))
+            return
+        elif msg.data > expected_n_cluster:
+            cfg['tolerance'] += delta
+        else:
+            cfg['tolerance'] -= delta
+        print('''\
+Expected n_cluster: {0}
+Actual   n_cluster: {1}
+tolerance: {2} -> {3}
+'''.format(expected_n_cluster, msg.data, tol_orig, cfg['tolerance']))
+        client.update_configuration(cfg)
+
+
+if __name__ == '__main__':
+    rospy.init_node('k_dynamic_euclid_clutering')
+    reconfig_eps = rospy.get_param('~reconfig_eps', 0.2)
+    node_name = rospy.get_param('~node')
+    client = dynamic_reconfigure.client.Client(node_name)
+    sub_kcluster = rospy.Subscriber('~k_cluster', Int32Stamped, cb_kcluster)
+    sub_ncluster = rospy.Subscriber(
+        '{node}/cluster_num'.format(node=node_name), Int32Stamped, cb)
+    rospy.spin()
