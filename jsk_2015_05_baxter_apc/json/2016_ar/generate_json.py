c0ea82d8b9f50e0e915ed3549051896ef275fd05
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-02-29 05:43:34 +0900

    Update for target obj

--- jsk_2015_05_baxter_apc/json/2016_ar/generate_json.py
+++ jsk_2015_05_baxter_apc/json/2016_ar/generate_json.py
@@ -16,14 +16,23 @@ json_id = args.json_id
 
 N = 2
 target_bin = 'h'
+abandon_objects = [
+    'genuine_joe_plastic_stir_sticks',
+    'cheezit_big_original',
+    'rolodex_jumbo_pencil_cup',
+    'champion_copper_plus_spark_plug',
+    'oreo_mega_stuf',
+]
 objects = jsk_apc2015_common.get_object_list()
-json_data = {}
-candidates = []
-for i in xrange(N):
-    i_obj = random.randint(0, len(objects) - 1)
-    candidates.append(objects[i_obj])
-i_target = random.randint(0, len(candidates) - 1)
-target_obj = candidates[i_target]
+
+target_obj = None
+while (target_obj is None) or (target_obj in abandon_objects):
+    candidates = []
+    for i in xrange(N):
+        i_obj = random.randint(0, len(objects) - 1)
+        candidates.append(objects[i_obj])
+    i_target = random.randint(0, len(candidates) - 1)
+    target_obj = candidates[i_target]
 
 json_data = {
     'bin_contents': {
