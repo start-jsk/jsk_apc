a2faf43295d160cb6dae7988c6eaf12f48f59ee0
Heecheol Kim <kimhc6028@gmail.com>
2016-03-10 16:21:44 +0900

    reusing jsk_apc2015_common.visualize_json

--- jsk_apc2016_common/python/jsk_apc2016_common/__init__.py
+++ jsk_apc2016_common/python/jsk_apc2016_common/__init__.py
@@ -36,73 +36,21 @@ def load_stow_json(json_file):
     return bin_contents, work_order
 
 def visualize_stow_json(json_file):
-    """Visualize bin_contents and work_order with a json file"""
     from jsk_apc2015_common.util import rescale
-    # load data from json
     bin_contents, work_order = load_stow_json(json_file)
-    # initialize variables
-    kiva_pod_img = cv2.imread(osp.join(OLD_PKG_PATH, 'models/kiva_pod/image.jpg'))
+    kiva_pod_img = jsk_apc2015_common.visualize_json(json_file,bin_contents)
     tote_img = cv2.imread(osp.join(PKG_PATH, 'models/tote/image.jpg'))
-
-    BIN_REGION = {
-        'a': ((0, 50), (640, 610)),
-        'b': ((640, 50), (1410, 610)),
-        'c': ((1410, 50), (2060, 610)),
-        'd': ((0, 680), (640, 1200)),
-        'e': ((640, 680), (1410, 1200)),
-        'f': ((1410, 680), (2060, 1200)),
-        'g': ((0, 1280), (640, 1770)),
-        'h': ((640, 1280), (1410, 1770)),
-        'i': ((1410, 1280), (2060, 1770)),
-        'j': ((0, 1850), (640, 2430)),
-        'k': ((640, 1850), (1410, 2430)),
-        'l': ((1410, 1850), (2060, 2430)),
-    }
-    # get object images
-    object_list = get_object_data()
+    object_list = jsk_apc2015_common.get_object_list()
     object_imgs = {}
     for obj in object_list:
-        obj = obj['name']
         img_path = osp.join(OLD_PKG_PATH, 'models/{obj}/image.jpg'.format(obj=obj))
         img = cv2.imread(img_path)
         h, w = img.shape[:2]
         if h > w:
             img = np.rollaxis(img, 1)
         object_imgs[obj] = img
-    # draw objects
-    for bin, contents in bin_contents.items():
-        bin_pt1, bin_pt2 = BIN_REGION[bin]
-        bin_region = kiva_pod_img[bin_pt1[1]:bin_pt2[1], bin_pt1[0]:bin_pt2[0]]
-        x_num, y_num = jsk_apc2015_common._get_tile_shape(len(contents))
-        bin_h, bin_w = bin_region.shape[:2]
-        max_obj_h, max_obj_w = bin_h // y_num, bin_w // x_num
-        for i_y in xrange(y_num):
-            y_min = int(1. * bin_h / y_num * i_y)
-            for i_x in xrange(x_num):
-                x_min = int(1. * bin_w / x_num * i_x)
-                if contents:
-                    obj = contents.pop()
-                    obj_img = object_imgs[obj]
-                    scale_h = 1. * max_obj_h / obj_img.shape[0]
-                    scale_w = 1. * max_obj_w / obj_img.shape[1]
-                    scale = min([scale_h, scale_w])
-                    obj_img = rescale(obj_img, scale)
-                    obj_h, obj_w = obj_img.shape[:2]
-                    x_max, y_max = x_min + obj_w, y_min + obj_h
-                    bin_region[y_min:y_max, x_min:x_max] = obj_img
 
-
-    # draw bin regions
-    for bin, region in BIN_REGION.items():
-        bin_pt1, bin_pt2 = region
-        bin_h, bin_w = bin_pt2[1] - bin_pt1[1], bin_pt2[0] - bin_pt1[0]
-        cv2.rectangle(kiva_pod_img, bin_pt1, bin_pt2, (0, 0, 255), 2)
-        text_pos = (bin_pt2[0] - bin_h//2 - 100, bin_pt2[1])
-        cv2.putText(kiva_pod_img, bin.upper(), text_pos,
-                    cv2.FONT_HERSHEY_PLAIN, 10, (0, 0, 255), 3)
-    
-
-    # draw objects in tote
+  # draw objects in tote
     tote_region = [[190,230],[1080,790]]
     region_h = tote_region[1][1] - tote_region[0][1]
     region_w = tote_region[1][0] - tote_region[0][0]
