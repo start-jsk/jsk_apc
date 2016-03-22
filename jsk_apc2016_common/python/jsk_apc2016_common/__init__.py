def809279d1c96d0a67a11cdc52ee428db5fa637
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-03-22 18:38:58 +0900

    Add roslint_test for python library

--- jsk_apc2016_common/python/jsk_apc2016_common/__init__.py
+++ jsk_apc2016_common/python/jsk_apc2016_common/__init__.py
@@ -53,11 +53,11 @@ def visualize_stow_contents(work_order):
             img = np.rollaxis(img, 1)
         object_imgs[obj] = img
     # draw object images on tote image
-    tote_region = [[190,230],[1080,790]]
+    tote_region = [[190, 230], [1080, 790]]
     region_h = tote_region[1][1] - tote_region[0][1]
     region_w = tote_region[1][0] - tote_region[0][0]
-    max_obj_h,max_obj_w = region_h / 3, region_w / 4
-    tote_x_min,tote_y_min = tote_region[0][0], tote_region[0][1]
+    max_obj_h, max_obj_w = region_h / 3, region_w / 4
+    tote_x_min, tote_y_min = tote_region[0][0], tote_region[0][1]
     x_min, y_min = tote_x_min, tote_y_min
     for obj in work_order:
         obj_img = object_imgs[obj]
@@ -69,7 +69,7 @@ def visualize_stow_contents(work_order):
         x_max, y_max = x_min + obj_w, y_min + obj_h
         tote_img[y_min:y_max, x_min:x_max] = obj_img
         x_min += max_obj_w
-        if x_max >= region_w :
+        if x_max >= region_w:
             x_min = tote_x_min
             y_min += max_obj_h
     return tote_img
@@ -103,8 +103,8 @@ def visualize_stow_json(json_file):
     tote_img = visualize_stow_contents(work_order)
 
     # merge two images
-    kiva_w,kiva_h = kiva_pod_img.shape[1],kiva_pod_img.shape[0]
-    tote_w,tote_h = tote_img.shape[1],tote_img.shape[0]
-    tote_img = cv2.resize(tote_img,(kiva_w,tote_h*kiva_w//tote_w))
-    dest = np.concatenate((kiva_pod_img,tote_img),axis=0)
+    kiva_w = kiva_pod_img.shape[1]
+    tote_w, tote_h = tote_img.shape[1], tote_img.shape[0]
+    tote_img = cv2.resize(tote_img, (kiva_w, tote_h*kiva_w//tote_w))
+    dest = np.concatenate((kiva_pod_img, tote_img), axis=0)
     return dest
