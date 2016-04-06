01b996ba5bdcec19119398b43d7c79da58e9427f
banmasa <bowofvinderre@gmail.com>
2016-04-06 21:06:09 +0900

    added servo state controller in arduino firmware

--- jsk_2016_01_baxter_apc/firmware/arm_manager.ino
+++ jsk_2016_01_baxter_apc/firmware/arm_manager.ino
@@ -21,21 +21,19 @@ int16_t dig_P6;
 int16_t dig_P7;
 int16_t dig_P8;
 int16_t dig_P9;
-int8_t  dig_H1;
-int16_t dig_H2;
-int8_t  dig_H3;
-int16_t dig_H4;
-int16_t dig_H5;
-int8_t  dig_H6;
 
 ros::NodeHandle nh; //write with IDE
 // ros::NodeHandle_<ArduinoHardware, 1, 2, 512, 512> nh;
 
 std_msgs::Float64 float_msg;
 std_msgs::Bool bool_msg;
+std_msgs::Bool servo_state_msg;
+std_msgs::Int16 servo_angle_msg;
 
 ros::Publisher pressure_pub("vacuum_gripper/limb/right/pressure", &float_msg);
 ros::Publisher state_pub("gripper_grabbed/limb/right/state", &bool_msg);
+ros::Publisher servo_state_pub("vacuum_gripper/limb/right/servo_state_info", &servo_state_msg);
+ros::Publisher servo_angle_pub("vacuum_gripper/limb/right/servo_angle_info", &servo_angle_msg);
 
 unsigned long  publisher_timer = 0;
 
@@ -43,25 +41,42 @@ Servo myservo;
 
 void servoCb(const std_msgs::Int16& angle_msg)
 {
-    int angle;
-    angle = angle_msg.data;
-    if (angle < 0) angle = 0;
-    if (angle > 180) angle = 180;
-
-    myservo.write(angle);
+    if (servo_state_msg.data == false) return;
+    if (angle_msg.data < 0) servo_angle_msg.data = 0;
+    else if (angle_msg.data > 180) servo_angle_msg.data = 180;
+    else servo_angle_msg.data = angle_msg.data;
+    myservo.write(servo_angle_msg.data);
 }
 
-ros::Subscriber<std_msgs::Int16> servo_sub("vacuum_gripper/limb/right/servo", &servoCb);
+void servo_toggleCb(const std_msgs::Bool& servo_toggle_msg)
+{
+    if (servo_toggle_msg.data) {
+        servo_state_msg.data = true;
+        if (!myservo.attached()) myservo.attach(5);
+        myservo.write(servo_angle_msg.data);
+    } else {
+        servo_state_msg.data = false;
+        if (myservo.attached()) myservo.detach();
+    }
+}
+
+ros::Subscriber<std_msgs::Int16> servo_angle_sub("vacuum_gripper/limb/right/servo_angle", &servoCb);
+ros::Subscriber<std_msgs::Bool> servo_state_sub("vacuum_gripper/limb/right/servo_state", &servo_toggleCb);
 
 void setup() {
+    servo_state_msg.data = false;
+    servo_angle_msg.data = 90;
     init_servo();
     pinMode(SS, OUTPUT);
     digitalWrite(SS, HIGH);
-    nh.getHardware()->setBaud(57600);
+    nh.getHardware()->setBaud(115200);
     nh.initNode();
-    nh.subscribe(servo_sub);
+    nh.subscribe(servo_angle_sub);
+    nh.subscribe(servo_state_sub);
     nh.advertise(pressure_pub);
     nh.advertise(state_pub);
+    nh.advertise(servo_state_pub);
+    nh.advertise(servo_angle_pub);
 
     SPI.begin();
     SPI.setClockDivider(SPI_CLOCK_DIV8);
@@ -86,6 +101,8 @@ void loop() {
 
         state_pub.publish(&bool_msg);
         pressure_pub.publish(&float_msg);
+        servo_state_pub.publish(&servo_state_msg);
+        servo_angle_pub.publish(&servo_angle_msg);
 
         publisher_timer = millis() + 100;
     }
@@ -95,6 +112,7 @@ void loop() {
 void init_servo()
 {
     myservo.attach(5);
+    myservo.write(servo_angle_msg.data);
 }
 
 void initBME()
@@ -142,12 +160,6 @@ void readTrim()
     dig_P7 = (data[19] << 8) | data[18];
     dig_P8 = (data[21] << 8) | data[20];
     dig_P9 = (data[23] << 8) | data[22];
-    dig_H1 = data[24];
-    dig_H2 = (data[26] << 8) | data[25];
-    dig_H3 = data[27];
-    dig_H4 = (data[28] << 4) | (0x0F & data[29]);
-    dig_H5 = (data[30] << 4) | ((data[29] >> 4) & 0x0F);
-    dig_H6 = data[31];
 }
 
 void readData()
