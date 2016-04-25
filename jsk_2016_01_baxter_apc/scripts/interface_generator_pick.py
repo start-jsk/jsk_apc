b2b73c1606ccf353df6ceb36bab1d91071981538
Shingo Kitagawa <shingogo@hotmail.co.jp>
2016-04-25 14:05:42 +0900

    interface-generator fixed into random

--- jsk_2016_01_baxter_apc/scripts/interface_generator_pick.py
+++ jsk_2016_01_baxter_apc/scripts/interface_generator_pick.py
@@ -1,56 +1,10 @@
 #!/usr/bin/env python
 
-#/*********************************************************************
-# *
-# * Software License Agreement (BSD License)
-# *
-# *  Copyright (c) 2015, Kiva Systems
-# *  All rights reserved.
-# *
-# *  Redistribution and use in source and binary forms, with or without
-# *  modification, are permitted provided that the following conditions
-# *  are met:
-# *
-# *   * Redistributions of source code must retain the above copyright
-# *     notice, this list of conditions and the following disclaimer.
-# *   * Redistributions in binary form must reproduce the above
-# *     copyright notice, this list of conditions and the following
-# *     disclaimer in the documentation and/or other materials provided
-# *     with the distribution.
-# *   * Neither the name of the authors nor the names of the
-# *     contributors may be used to endorse or promote products derived
-# *     from this software without specific prior written permission.
-# *
-# *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# *  POSSIBILITY OF SUCH DAMAGE.
-# *
-# *********************************************************************/
-
-
-# This is demo program of pick task json file generator
-'''
-Usage : python interface_generator_pick.py {version_of_pick}
-ex) python interface_generator_pick.py 2 => generates pick_layout_2.json at ../json directory
-'''
 import copy
 import json
 import random
 import os
 import jsk_apc2016_common
-#-------------------------------------------------------------------------------
-
-
-# define our bin and item names to use
 
 CONST_BIN_NAMES = ['bin_A',
                    'bin_B',
@@ -65,11 +19,7 @@ CONST_BIN_NAMES = ['bin_A',
                    'bin_K',
                    'bin_L']
 
-NBINS = len(CONST_BIN_NAMES)
-N1_2_BINS = 3 + random.randint(0,1)
-N3_4_BINS = 5 + random.randint(0,1)
-N5__BINS = NBINS - (N1_2_BINS + N3_4_BINS) #assumed to be more than 5 items
-N_TOTAL_ITEMS = 47  ###CHANGE THIS TO 50 AFTER FULL ITEMS BE DELIEVERED FROM AMAZON
+N_TOTAL_ITEMS = 47
 
 ITEMS_DATA = jsk_apc2016_common.get_object_data()
 CONST_ITEM_NAMES = []
@@ -80,99 +30,39 @@ for item_data in ITEMS_DATA :
 
 class InterfaceGeneratorPick():
     def __init__ (self):
-        self.count_items = N_TOTAL_ITEMS
-        self.bin_contents = {bin_name:[] for bin_name in CONST_BIN_NAMES}
-        self.bin_list = [1] * NBINS
-        self.bin_check = [False] * NBINS
-        self.items_bins = copy.deepcopy(CONST_ITEM_NAMES) # create a destroyable copy of the items
+        self.total_item_num = N_TOTAL_ITEMS
+        self.bin_names = copy.deepcopy(CONST_BIN_NAMES)
+        self.bin_contents = {bin_name:[] for bin_name in self.bin_names}
+        self.items = copy.deepcopy(CONST_ITEM_NAMES)
         self.items_stock = copy.deepcopy(CONST_N_ITEMS)
-        self.abins = copy.deepcopy(CONST_BIN_NAMES) # create a destroyable copy of the bins
+
+    def gen_random_item_list(self):
+        self.item_list = []
+        for item, stock in zip(self.items, self.items_stock):
+            for i in range(0, stock):
+                self.item_list.append(item)
+        random.shuffle(self.item_list)
+        self.item_list[:self.total_item_num]
+
+    def select_bin(self):
+        while True:
+            item_list_bin =[]
+            for i in range(0, self.total_item_num):
+                item_list_bin.append(random.choice(self.bin_names))
+            if set(self.bin_names) <= set(item_list_bin):
+                break
+        self.item_list_bin = item_list_bin
+
+    def gen_bin_contents(self):
+        for bin_name, item in zip(self.item_list_bin, self.item_list):
+            self.bin_contents[bin_name].append(item)
 
     def run(self):
-        self.num_items_bin()
-        self.select_item_bin()
+        self.gen_random_item_list()
+        self.select_bin()
+        self.gen_bin_contents()
         self.gen_workorder()
-
-
-    # number of items stored at each bin
-    def num_items_bin(self):
-        for i in range(N5__BINS):
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == False:
-                    break
-            n_item = random.randint(5,10)
-            self.bin_list[index] = n_item
-            self.count_items -= n_item
-            self.bin_check[index] = True
-
-        for i in range(N3_4_BINS):
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == False:
-                    break
-            n_item = random.randint(3,4)
-            self.bin_list[index] = n_item
-            self.count_items -= n_item
-            self.bin_check[index] = True
-
-        while self.count_items > 6:
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == True:
-                    break
-            self.bin_list[index] += 1
-            self.count_items -= 1
-
-        while self.count_items < N1_2_BINS:
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if (self.bin_check[index] == True) and (self.bin_list[index] > 1):
-                    break
-            self.bin_list[index] -= 1
-            self.count_items += 1
-
-        n_1 = - self.count_items + 2 * N1_2_BINS
-        n_2 =  self.count_items - N1_2_BINS
-
-        for i in range(n_2):
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == False:
-                    break
-            n_item = 2
-            self.bin_list[index] = n_item
-
-        if not sum(self.bin_list) == N_TOTAL_ITEMS:
-            print "warning : number of items unmatched. Recommended to try again"
-            print "number of total items should be: {}".format(N_TOTAL_ITEMS)
-            print "sum of items in bin: {}".format(sum(self.bin_list))
-
-    # generate all item bins
-    def select_item_bin(self):
-        abins_ = copy.deepcopy(self.abins)
-        bin_contents_ = copy.deepcopy(self.bin_contents)
-        for i in range(0,NBINS):
-            bin_name = random.choice(abins_)
-            abins_.remove(bin_name)
-            item_list = []
-            items_stock_ = copy.deepcopy(self.items_stock)
-            for j in range(0,self.bin_list[i]):
-                while True:
-                    item_index = random.randint(0,len(self.items_bins) - 1)
-                    if (items_stock_[item_index] > 0):
-                        item_name = self.items_bins[item_index]
-                        items_stock_[item_index] -= 1
-                        break
-                item_list.append(item_name)
-            bin_contents_[bin_name] = item_list
-        self.abins = abins_
-        self.items_stock = items_stock_
-        self.bin_contents = bin_contents_
-        no_items = [x for x in self.items_stock if x < 0]
-        if len(no_items) > 0:
-            print "warning: not enough items expected for this json. Please try again"
-
+    
     # generate the work order data structure
     def gen_workorder(self):
         self.work_order = [{'bin':bin_name,'item':item_name} for bin_name in CONST_BIN_NAMES
@@ -186,11 +76,11 @@ class InterfaceGeneratorPick():
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         os.chdir(output_dir)
-        i=1
+        i = 1
         while True:
             filename  = 'pick_layout_'+str(i)+'.json' 
             if os.path.isfile(filename):
-                i +=1
+                i += 1
             else:
                 with open(filename, 'w') as outfile:
                     json.dump(data, outfile, sort_keys=True, indent=4, separators=(',',': '))
--- jsk_2016_01_baxter_apc/scripts/interface_generator_stow.py
+++ jsk_2016_01_baxter_apc/scripts/interface_generator_stow.py
@@ -1,55 +1,11 @@
 #!/usr/bin/env python
 
-#/*********************************************************************
-# *
-# * Software License Agreement (BSD License)
-# *
-# *  Copyright (c) 2015, Kiva Systems
-# *  All rights reserved.
-# *
-# *  Redistribution and use in source and binary forms, with or without
-# *  modification, are permitted provided that the following conditions
-# *  are met:
-# *
-# *   * Redistributions of source code must retain the above copyright
-# *     notice, this list of conditions and the following disclaimer.
-# *   * Redistributions in binary form must reproduce the above
-# *     copyright notice, this list of conditions and the following
-# *     disclaimer in the documentation and/or other materials provided
-# *     with the distribution.
-# *   * Neither the name of the authors nor the names of the
-# *     contributors may be used to endorse or promote products derived
-# *     from this software without specific prior written permission.
-# *
-# *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# *  POSSIBILITY OF SUCH DAMAGE.
-# *
-# *********************************************************************/
-
-
-# This is demo program of stow task json file generator
-'''
-Usage : python interface_generator_stow.py
-ex) python interface_generator_pick.py 2 => generates pick_layout_2.json at ../json directory
-'''
-
 import copy
 import json
 import random
 import os
 import jsk_apc2016_common
 
-# define our bin and item names to use
 CONST_BIN_NAMES = ['bin_A',
                    'bin_B',
                    'bin_C',
@@ -64,12 +20,8 @@ CONST_BIN_NAMES = ['bin_A',
                    'bin_L']
 
 
-NBINS = len(CONST_BIN_NAMES)
-N1_2_BINS = 3 + random.randint(0,1)
-N3_4_BINS = 5 + random.randint(0,1)
-N5__BINS = NBINS - (N1_2_BINS + N3_4_BINS) #assumed to be more than 5 items
-NTOTE_TOTAL = 12 # total number of items
-N_TOTAL_ITEMS = 35 ###CHANGE THIS TO 40 AFTER FULL ITEMS BE DELIEVERED FROM AMAZON
+NTOTE_TOTAL = 12
+N_TOTAL_ITEMS = 35
 
 ITEMS_DATA = jsk_apc2016_common.get_object_data()
 CONST_ITEM_NAMES = []
@@ -79,119 +31,49 @@ for item_data in ITEMS_DATA :
     CONST_N_ITEMS.append(item_data['stock'])
 
 class InterfaceGeneratorStow():
-
     def __init__(self):
-    # generate the bin contents data structure
-    #-------------------------------------------------------------------------------
-        self.count_items = N_TOTAL_ITEMS
+        self.total_item_num = N_TOTAL_ITEMS
+        self.bin_names = copy.deepcopy(CONST_BIN_NAMES)
         self.bin_contents = {bin_name:[] for bin_name in CONST_BIN_NAMES}
-        self.tote_contents = []
-        self.bin_list = [1] * len(CONST_BIN_NAMES)
-        self.bin_check = [False] * len(CONST_BIN_NAMES)
-        self.items_bins = copy.deepcopy(CONST_ITEM_NAMES) # create a destroyable copy of the items
+        self.items = copy.deepcopy(CONST_ITEM_NAMES) # create a destroyable copy of the items
         self.items_stock = copy.deepcopy(CONST_N_ITEMS)
-        self.abins = copy.deepcopy(CONST_BIN_NAMES) # create a destroyable copy of the bins
 
-    def run(self):
-        self.select_tote()
-        self.num_items_bin()
-        self.select_item_bin()
+    def gen_random_item_list(self):
+        self.item_list = []
+        for item, stock in zip(self.items, self.items_stock):
+            for i in range(0, stock):
+                self.item_list.append(item)
+        random.shuffle(self.item_list)
+        self.item_list[:self.total_item_num]
+
+    def select_bin(self):
+        while True:
+            item_list_bin =[]
+            for i in range(0, self.total_item_num):
+                item_list_bin.append(random.choice(self.bin_names))
+            if set(self.bin_names) <= set(item_list_bin):
+                break
+        self.item_list_bin = item_list_bin
+
+    def gen_bin_contents(self):
+        for bin_name, item in zip(self.item_list_bin, self.item_list):
+            self.bin_contents[bin_name].append(item)
 
     def select_tote(self):
-        # generate tote items. Some items are chosen multiple times
-        for i in range(0,NTOTE_TOTAL):
+        self.tote_contents = []
+        for i in range(0, NTOTE_TOTAL):
             item_name = self.select_tote_item()
             self.tote_contents.append(item_name)
 
-    def num_items_bin(self):
-        for i in range(N5__BINS):
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == False:
-                    break
-            n_item = random.randint(5,10)
-            self.bin_list[index] = n_item
-            self.count_items -= n_item
-            self.bin_check[index] = True
-
-        for i in range(N3_4_BINS):
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == False:
-                    break
-            n_item = random.randint(3,4)
-            self.bin_list[index] = n_item
-            self.count_items -= n_item
-            self.bin_check[index] = True
-
-        while self.count_items > 6:
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == True:
-                    break
-            self.bin_list[index] += 1
-            self.count_items -= 1
-
-        while self.count_items < N1_2_BINS:
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if (self.bin_check[index] == True) and (self.bin_list[index] > 1):
-                    break
-            self.bin_list[index] -= 1
-            self.count_items += 1
-
-        n_1 = - self.count_items + 2 * N1_2_BINS
-        n_2 =  self.count_items - N1_2_BINS
-
-        for i in range(n_2):
-            while True:
-                index = random.randint(0, NBINS - 1)
-                if self.bin_check[index] == False:
-                    break
-            n_item = 2
-            self.bin_list[index] = n_item
-
-        if not sum(self.bin_list) == N_TOTAL_ITEMS:
-            print "warning : number of items unmatched. Recommended to try again"
-            print "number of total items should be: {}".format(N_TOTAL_ITEMS)
-            print "sum of items in bin: {}".format(sum(self.bin_list))
-
-    # generate all item bins
-    def select_item_bin(self):
-        abins_ = copy.deepcopy(self.abins)
-        bin_contents_ = copy.deepcopy(self.bin_contents)
-        for i in range(0,NBINS):
-            bin_name = random.choice(abins_)
-            abins_.remove(bin_name)
-            item_list = []
-            items_stock_ = copy.deepcopy(self.items_stock)
-            for j in range(0,self.bin_list[i]):
-                while True:
-                    item_index = random.randint(0,len(self.items_bins) - 1)
-                    if (items_stock_[item_index] > 0):
-                        item_name = self.items_bins[item_index]
-                        items_stock_[item_index] -= 1
-                        break
-                item_list.append(item_name)
-            bin_contents_[bin_name] = item_list
-        self.abins = abins_
-        self.items_stock = items_stock_
-        self.bin_contents = bin_contents_
-
-        no_items = [x for x in self.items_stock if x < 0]
-        if len(no_items) > 0:
-            print "warning: not enough items expected for this json. Please try again"
-
     def select_tote_item(self):
         while True:
-            item_index = random.randint(0,len(self.items_bins) - 1)
+            item_index = random.randint(0, len(self.items) - 1)
             if (self.items_stock[item_index] > 0):
-                item_name = self.items_bins[item_index]
+                item_name = self.items[item_index]
                 self.items_stock[item_index] -= 1
                 break
         return item_name
 
-    # write the dictionary to the appropriately names json file
     def write_json(self):
         data = {'bin_contents': self.bin_contents, 'work_order': self.tote_contents}
         this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -210,6 +92,12 @@ class InterfaceGeneratorStow():
                 print(filename+ ' generated at ../json')
                 os.chdir(this_dir)
                 break
+
+    def run(self):
+        self.select_tote()
+        self.gen_random_item_list()
+        self.select_bin()
+        self.gen_bin_contents()
 
 if __name__ == "__main__":
     interface_stow = InterfaceGeneratorStow()
