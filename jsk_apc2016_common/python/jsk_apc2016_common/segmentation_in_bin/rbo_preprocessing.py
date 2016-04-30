24230c2edd43b8f8e1c3e25a5732e86eea95c2fa
Yusuke Niitani <yuyuniitani@gmail.com>
2016-04-30 18:19:27 +0900

    add spatial feature extractions

--- jsk_apc2016_common/python/jsk_apc2016_common/segmentation_in_bin/rbo_preprocessing.py
+++ jsk_apc2016_common/python/jsk_apc2016_common/segmentation_in_bin/rbo_preprocessing.py
@@ -83,3 +83,59 @@ def get_spatial(cloud, bbox, trans, direction):
     dist_list = [p_info[0] for p_info in spatial_list]
     height_list = [p_info[1] for p_info in spatial_list]
     return dist_list, height_list
+
+
+def get_mask_img(transform, target_bin, camera_model):
+    """
+    :param point: point that is going to be transformed
+    :type point: PointStamped
+    :param transform: camera_frame -> bbox_frame
+    :type transform: Transform
+    """
+    # check frame_id of a point and transform just in case
+    assert camera_model.tf_frame == transform.header.frame_id
+    assert target_bin.bbox.header.frame_id == transform.child_frame_id
+
+    transformed_list = [
+            do_transform_point(corner, transform)
+            for corner in target_bin.corners]
+    projected_points = project_points(transformed_list, camera_model)
+
+    # generate an polygon that covers the region
+    path = Path(projected_points)
+    x, y = np.meshgrid(
+            np.arange(camera_model.width),
+            np.arange(camera_model.height))
+    x, y = x.flatten(), y.flatten()
+    points = np.vstack((x, y)).T
+    mask_img = path.contains_points(
+            points).reshape(
+                    camera_model.height, camera_model.width
+                ).astype('bool')
+    return mask_img
+
+
+def project_points(points, camera_model):
+    """
+    :param points: list of geometry_msgs.msg.PointStamped
+    :type list of stamped points :
+    :param projected_points: list of camera_coordinates
+    :type  projected_points: (u, v)
+
+    The frames of the points and the camera_model are same.
+    """
+    # generate mask iamge
+    for point in points:
+        if point.header.frame_id != camera_model.tf_frame:
+            raise ValueError('undefined')
+    if len(points) != 4:
+        raise ValueError('undefined')
+
+    projected_points = []
+    for point in points:
+        projected_points.append(
+                camera_model.project3dToPixel(
+                        helper.list_from_point(point.point)
+                    )
+            )
+    return projected_points
