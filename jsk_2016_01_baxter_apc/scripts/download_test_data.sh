821d739e1ec343ca2b29b5cf8f5dd85ded9e7f2c
Yusuke Niitani <yuyuniitani@gmail.com>
2016-05-18 01:32:46 +0900

    add download script for test data

--- jsk_2016_01_baxter_apc/scripts/download_test_data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-DATA_DIR=$(rospack find jsk_2016_01_baxter_apc)/test_data
-
-gdown "https://drive.google.com/uc?id=0B9P1L--7Wd2vZ2xLZG55OWNYTDQ" -O $DATA_DIR/2016-04-30-16-33-54_apc2016-bin-boxes.bag
