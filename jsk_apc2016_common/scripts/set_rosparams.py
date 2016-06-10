f9bdbda4e9fbe1424af6a70b1f49c747d12c3f6a
Yusuke Niitani <yuyuniitani@gmail.com>
2016-06-10 21:29:12 +0900

    set_rosparam change

--- jsk_apc2016_common/scripts/set_rosparams.py
+++ jsk_apc2016_common/scripts/set_rosparams.py
@@ -10,9 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('json')
 args = parser.parse_args()
 
-vis_json_cmd = sh.Command('rosparam', 'set', '/visualize_json/json', args.json)
-set_bin_param_cmd = sh.Command(
-    'rosparam', 'set', '/set_bin_param/json', args.json)
+rosparam_cmd = sh.Command('rosparam')
 
-vis_json_cmd()
-set_bin_param_cmd()
+rosparam_cmd('set', '/visualize_json/json', args.json)
+rosparam_cmd('set', '/set_bin_param/json', args.json)
