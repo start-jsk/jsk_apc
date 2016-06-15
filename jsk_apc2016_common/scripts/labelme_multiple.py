7b701c517c011fc809c749a2f862121e08cb82ce
Yusuke Niitani <yuyuniitani@gmail.com>
2016-06-16 00:01:56 +0900

    update labelme

--- jsk_apc2016_common/scripts/labelme_multiple.py
+++ jsk_apc2016_common/scripts/labelme_multiple.py
@@ -4,6 +4,7 @@ import sh
 import argparse
 import os
 import pickle
+import shutil
 
 import json
 
@@ -13,6 +14,18 @@ import PIL.Image
 import PIL.ImageDraw
 import numpy as np
 import cv2
+
+
+"""Script to label multiple image sets using LabelMe
+
+Usage:
+    Create polygons --> Save --> (Next image set) --> Create polygons
+
+    If you want to delete the displayed dataset, put label '-1'.
+
+    You may want to skip labelling some images. In that case, you can just 
+    close Labelme.
+"""
 
 
 object_names = [
@@ -34,54 +47,125 @@ for i, name in enumerate(object_names):
     object_names_dict[name] = i
 
 
-def print_target_objects(prefix):
-    with open(prefix + '.pkl', 'rb') as f:
-        data = pickle.load(f)
-    for _object in data['objects']:
-        print 'id: {},  {}'.format(object_names_dict[_object], _object)
+class LabelMeMultiple(object):
+    def __init__(self, prefix, labeled_dir):
+        self.prefix = prefix
+        self.labeled_dir = labeled_dir
+
+        self.json_path = prefix + '.json'
+        self.img_path = prefix + '.jpg'
+        self.mask_path = prefix + '.pbm'
+        self.pkl_path = prefix + '.pkl'
+        self.input_paths = [
+            self.json_path, self.img_path, self.mask_path, self.pkl_path]
+
+    def load_data(self):
+        with open(self.json_path, 'rb') as f:
+            self.json_data = json.load(f)
+
+        with open(self.pkl_path, 'rb') as f:
+            self.data = pickle.load(f)
+
+    def _convert_to_path_labeled(self, path):
+        return self.labeled_dir + os.path.split(path)[1]
+
+    def print_target_objects(self):
+        for _object in self.data['objects']:
+            print 'id: {},  {}'.format(object_names_dict[_object], _object)
+
+    def convert_id_to_object_name(self):
+        """Modify label name of json from ids to object names
+        """
+        for shape in self.json_data['shapes']:
+            label = shape['label']
+            shape['label'] = object_names[int(label)]
+        with open(self.json_path, 'w') as f:
+            f.write(json.dumps(self.json_data, indent=2))
+
+    def check_delete_command(self):
+        for shape in self.json_data['shapes']:
+            if shape['label'] == '-1':
+                print 'deleting images'
+                return True
+        return False
+
+    def delete_this_set(self):
+        for delete_item_path in self.input_paths:
+            os.remove(delete_item_path)
+
+    def move_data_to_labeled(self):
+        if not os.path.exists(self.labeled_dir):
+            print 'entered'
+            os.makedirs(self.labeled_dir, mode=0755)
+        for input_path in self.input_paths:
+            output_path = self._convert_to_path_labeled(input_path)
+            shutil.move(input_path, output_path)
+
+    def save_bin_mask(self):
+        """save all labels in json_file as binary mask images
+        """
+        f = StringIO()
+        f.write(base64.b64decode(self.json_data['imageData']))
+        img = np.array(PIL.Image.open(f))
+
+        target_names = {'background': 0}
+        label = np.zeros(img.shape[:2], dtype=np.int32)
+        for shape in self.json_data['shapes']:
+            # get label value
+            label_value = target_names.get(shape['label'])
+            if label_value is None:
+                label_value = len(target_names)
+                target_names[shape['label']] = label_value
+            # polygon -> mask
+            mask = np.zeros(img.shape[:2], dtype=np.uint8)
+            mask = PIL.Image.fromarray(mask)
+            xy = map(tuple, shape['points'])
+            PIL.ImageDraw.Draw(mask).polygon(xy=xy, outline=1, fill=1)
+            mask = np.array(mask)
+            # fill label value
+            label[mask == 1] = label_value
+            file_name = self.prefix + '_' + shape['label'] + '.pbm'
+            cv2.imwrite(
+                file_name, 255 * (label == label_value).astype(np.uint8))
+            self.input_paths.append(file_name)
+
+    def __call__(self):
+        with open(self.pkl_path, 'rb') as f:
+            self.data = pickle.load(f)
+        if os.path.exists(self.json_path):
+            print 'SKIPPING because json alreadly exists'
+            return
+
+        print '\n\n'
+        print 'working on......  ', self.prefix
+        print '=============================================================='
+        self.print_target_objects()
+
+        # create label json
+        labelme_cmd = sh.Command('labelme')
+        labelme_cmd('-O', self.json_path, self.img_path)
+
+        if not os.path.isfile(self.json_path):
+            print 'exiting'
+            return
 
 
-def convert_id_to_object_name(json_path):
-    """Modify label name of json from ids to object names
+        # load json data and pkl data
+        self.load_data()
 
-    Args:
-        json_path (str): path to a json file that is going to be modified
-    """
-    with open(json_path, 'rb') as f:
-        json_data = json.load(f)
-    for shape in json_data['shapes']:
-        label = shape['label']
-        shape['label'] = object_names[int(label)]
-    with open(json_path, 'w') as f:
-        f.write(json.dumps(json_data, indent=2))
+        # check if delete message is in json
+        if self.check_delete_command():
+            self.delete_this_set()
+            return
 
+        # convert labeled ids to valid object names
+        self.convert_id_to_object_name()
 
-def save_bin_mask(json_file):
-    """save all labels in json_file as binary mask images
-    """
-    data = json.load(open(json_file))
-    f = StringIO()
-    f.write(base64.b64decode(data['imageData']))
-    img = np.array(PIL.Image.open(f))
+        # create binary mask from json
+        self.save_bin_mask()
 
-    target_names = {'background': 0}
-    label = np.zeros(img.shape[:2], dtype=np.int32)
-    for shape in data['shapes']:
-        # get label value
-        label_value = target_names.get(shape['label'])
-        if label_value is None:
-            label_value = len(target_names)
-            target_names[shape['label']] = label_value
-        # polygon -> mask
-        mask = np.zeros(img.shape[:2], dtype=np.uint8)
-        mask = PIL.Image.fromarray(mask)
-        xy = map(tuple, shape['points'])
-        PIL.ImageDraw.Draw(mask).polygon(xy=xy, outline=1, fill=1)
-        mask = np.array(mask)
-        # fill label value
-        label[mask == 1] = label_value
-        file_name = json_file[:-5] + '_objects_' + shape['label'] + '.pbm'
-        cv2.imwrite(file_name, 255 * (label == label_value).astype(np.uint8))
+        # move data to a directory for labeled images
+        self.move_data_to_labeled()
 
 
 def main():
@@ -89,25 +173,17 @@ def main():
     parser.add_argument('path')
     args = parser.parse_args()
 
+    labeled_dir = args.path + '_labeled/'
+
     ls_stdout = sh.ls(args.path).stdout
     ls_results = ls_stdout.split()
 
     color_results = [result for result in ls_results if '.jpg' in result]
 
     for result in color_results:
-        json_path = os.path.join(args.path, result[:-4] + '.json')
-        labelme_cmd = sh.Command('labelme')
-        print '\n\n'
-        print 'working on......  ', result[-9:-4]
-        print '=============================================================='
-        print_target_objects(os.path.join(args.path, result[:-4]))
-
-        # create label json
-        labelme_cmd('-O', json_path, os.path.join(args.path, result))
-        convert_id_to_object_name(json_path)
-
-        # create binary mask from json
-        save_bin_mask(json_path)
+        prefix = os.path.join(args.path, result[:-4])
+        label_me_multiple = LabelMeMultiple(prefix, labeled_dir)
+        label_me_multiple()
 
 
 if __name__ == '__main__':
