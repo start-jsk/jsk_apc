e47946c25537cddac44f0be32abc874bd65f81a8
pazeshun <pazeshun3684@gmail.com>
2016-06-17 01:19:43 +0900

    Push gripper joint states back of other joint states

--- jsk_2016_01_baxter_apc/src/gripper_joint_states_publisher.cpp
+++ jsk_2016_01_baxter_apc/src/gripper_joint_states_publisher.cpp
@@ -5,10 +5,26 @@
 #include <std_msgs/Float32.h>
 
 std::map<std::string, double> g_servo_angle;
+std::map<std::string, std::string> g_side_to_jt;
+sensor_msgs::JointState g_joint_states_except_gripper;
 
 void angle_stateCb(const std_msgs::Float32::ConstPtr& sub_msg, const std::string& side)
 {
   g_servo_angle[side] = sub_msg->data;
+}
+
+void joint_statesCb(const sensor_msgs::JointState::ConstPtr& joint_states)
+{
+  for (std::map<std::string, std::string>::iterator pair = g_side_to_jt.begin(); pair != g_side_to_jt.end(); ++pair)
+  {
+    for (int i = 0; i < joint_states->name.size(); i++)
+    {
+      if (joint_states->name[i] == pair->second)
+        return;
+    }
+  }
+
+  g_joint_states_except_gripper = (*joint_states);
 }
 
 int main(int argc, char** argv)
@@ -20,20 +36,21 @@ int main(int argc, char** argv)
                                                                          10, boost::bind(&angle_stateCb, _1, "right"));
   ros::Subscriber left_servo_angle_sub = n.subscribe<std_msgs::Float32>("gripper_front/limb/left/servo/angle/state", 10,
                                                                         boost::bind(&angle_stateCb, _1, "left"));
+  ros::Subscriber joint_states_sub = n.subscribe("robot/joint_states", 10, joint_statesCb);
 
-  std::map<std::string, std::string> side_to_jt;
-  side_to_jt["right"] = "right_gripper_vacuum_pad_joint";
-  side_to_jt["left"] = "left_gripper_vacuum_pad_joint";
+  g_side_to_jt["right"] = "right_gripper_vacuum_pad_joint";
+  g_side_to_jt["left"] = "left_gripper_vacuum_pad_joint";
   ros::Rate r(30);
   while (ros::ok())
   {
     ros::spinOnce();
-    if (g_servo_angle.size() == side_to_jt.size())
+    if (g_servo_angle.size() == g_side_to_jt.size())
     {
       sensor_msgs::JointState pub_msg;
 
+      pub_msg = g_joint_states_except_gripper;
       pub_msg.header.stamp = ros::Time::now();
-      for (std::map<std::string, std::string>::iterator pair = side_to_jt.begin(); pair != side_to_jt.end(); ++pair)
+      for (std::map<std::string, std::string>::iterator pair = g_side_to_jt.begin(); pair != g_side_to_jt.end(); ++pair)
       {
         pub_msg.name.push_back(pair->second);
         pub_msg.position.push_back(g_servo_angle[pair->first]);
