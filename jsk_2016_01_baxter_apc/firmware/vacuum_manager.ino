5cfbb90cf898b8a68b214fe1f22ec135b3b8ed19
ban-masa <bowofvinderre@gmail.com>
2016-06-20 15:51:55 +0900

    added firmware of arduino which controls vacuum switch

---
+++ jsk_2016_01_baxter_apc/firmware/vacuum_manager.ino
@@ -0,0 +1,71 @@
+#include <ros.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+
+const int RIGHT = 5;
+const int LEFT = 6;
+
+ros::NodeHandle nh;
+
+void message_rightCb(const std_msgs::Bool& toggle_msg) {
+  if (toggle_msg.data) {
+    digitalWrite(RIGHT, HIGH);
+  } else {
+    digitalWrite(RIGHT, LOW);
+  }
+}
+
+void message_leftCb(const std_msgs::Bool& toggle_msg) {
+  if (toggle_msg.data) {
+    digitalWrite(LEFT, HIGH);
+  } else {
+    digitalWrite(LEFT, LOW);
+  }
+}
+
+ros::Subscriber<std_msgs::Bool> sub_right("/vacuum_gripper/limb/right", &message_rightCb);
+ros::Subscriber<std_msgs::Bool> sub_left("/vacuum_gripper/limb/left", &message_leftCb);
+
+std_msgs::String str_msg_right;
+std_msgs::String str_msg_left;
+
+ros::Publisher pub_right("/vacuum_gripper/limb/right/state", &str_msg_right);
+ros::Publisher pub_left("/vacuum_gripper/limb/left/state", &str_msg_left);
+
+unsigned long publisher_timer = 0;
+
+void setup()
+{
+  pinMode(RIGHT, OUTPUT);
+  pinMode(LEFT, OUTPUT);
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
+  nh.subscribe(sub_right);
+  nh.subscribe(sub_left);
+  nh.advertise(pub_right);
+  nh.advertise(pub_left);
+}
+
+void loop()
+{
+  if (millis() > publisher_timer) {
+    if (digitalRead(RIGHT)) {
+      str_msg_right.data = "ON";
+    } else {
+      str_msg_right.data = "OFF";
+    }
+    if (digitalRead(LEFT)) {
+      str_msg_left.data = "ON";
+    } else {
+      str_msg_left.data = "OFF";
+    }
+
+    pub_right.publish(&str_msg_right);
+    pub_left.publish(&str_msg_left);
+
+    publisher_timer = millis() + 100;
+  }
+  nh.spinOnce();
+}
+
