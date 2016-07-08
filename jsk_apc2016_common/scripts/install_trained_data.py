0298ace270749a6de89595a5bb566739dc63b16e
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-07-08 19:21:36 +0900

    Add vgg16 trained_data to download

--- jsk_apc2016_common/scripts/install_trained_data.py
+++ jsk_apc2016_common/scripts/install_trained_data.py
@@ -13,6 +13,13 @@ def main():
         md5='3c993d333cf554684b5162c9f69b20cf',
     )
 
+    download_data(
+        pkg_name=PKG,
+        path='trained_data/vgg16_rotation_translation_brightness_372000.chainermodel',
+        url='https://drive.google.com/open?id=0B9P1L--7Wd2veHZKRkFwZjRiZDQ',
+        md5='58a0e819ba141a34b1d68cc5e972615b',
+    )
+
 
 if __name__ == '__main__':
     main()
