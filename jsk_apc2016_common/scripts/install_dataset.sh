09d3b8117df8b3491eb1bba838231d3dbb3255d4
Yusuke Niitani <yuyuniitani@gmail.com>
2016-07-15 22:18:10 +0900

    reflected new data & organized all RBO format data & changed name of directory

--- jsk_apc2016_common/scripts/install_dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p $(rospack find jsk_apc2016_common)/data/tokyo_run
-gdown https://drive.google.com/uc?id=0BzBTxmVQJTrGR3BHVkRPck9wTEE -O $(rospack find jsk_apc2016_common)/data/tokyo_run/single_item_labeled.zip
-unzip $(rospack find jsk_apc2016_common)/data/tokyo_run/single_item_labeled.zip -d $(rospack find jsk_apc2016_common)/data/tokyo_run
-rm $(rospack find jsk_apc2016_common)/data/tokyo_run/single_item_labeled.zip
-
-rosrun jsk_apc2016_common train_segmenter_2016.py
