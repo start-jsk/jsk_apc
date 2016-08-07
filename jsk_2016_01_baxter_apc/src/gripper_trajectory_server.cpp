80e059cf31ca1a2745ac818c7e6e4d5f90879b0f
Kei Okada <kei.okada@gmail.com>
2016-08-07 16:16:29 +0900

    add minjerk class

--- jsk_2016_01_baxter_apc/src/gripper_trajectory_server.cpp
+++ jsk_2016_01_baxter_apc/src/gripper_trajectory_server.cpp
@@ -1,9 +1,8 @@
-#include <map>
-
 #include <ros/ros.h>
 #include <control_msgs/FollowJointTrajectoryAction.h>
 #include <actionlib/server/simple_action_server.h>
 #include <std_msgs/Float32.h>
+#include "minjerk.h"
 
 class GripperAction
 {
@@ -22,6 +21,7 @@ protected:
   ros::Subscriber gripper_servo_angle_sub_;
 
   float angle_state_;
+  MinJerk minjerk;
 public:
   GripperAction(std::string name, std::string side);
   void executeCB(const control_msgs::FollowJointTrajectoryGoalConstPtr& goal);
@@ -63,12 +63,42 @@ void GripperAction::executeCB(const control_msgs::FollowJointTrajectoryGoalConst
   ros::Time start_segment_time = start_time;
   float prev_rad = angle_state_;
 
-  // Loop until end of trajectory
+  // setup interpolator
+  std::vector<double> position_list, velocity_list, acceleration_list;
+  std::vector<double> time_list;
+  position_list.push_back(angle_state_);
+  velocity_list.push_back(0);
+  acceleration_list.push_back(0);
   for (int i = 0; i < goal->trajectory.points.size(); i++)
   {
-    ros::Duration segment_time = goal->trajectory.points[i].time_from_start;
-    if ( i > 0 )
-      segment_time = segment_time -  goal->trajectory.points[i-1].time_from_start;
+    position_list.push_back(goal->trajectory.points[i].positions[0]);
+    time_list.push_back(goal->trajectory.points[i].time_from_start.toSec());
+    //
+    double velocity = 0;
+    if ( i <  goal->trajectory.points.size() - 1 ) {
+      double d0 = position_list[i+1] - position_list[i];
+      double d1 = goal->trajectory.points[i+1].positions[0] - goal->trajectory.points[i].positions[0];
+      double t0 = time_list[i] - ((i>0)?time_list[i-1]:0);
+      double t1 = goal->trajectory.points[i+1].time_from_start.toSec() - goal->trajectory.points[i].time_from_start.toSec();
+      double v0 = d0/t0;
+      double v1 = d1/t1;
+      if ( v0 * v1 >= 0 ) {
+        velocity = 0.5 * (v0 + v1);
+      } else {
+        velocity = 0;
+      }
+    }
+    velocity_list.push_back(velocity);
+    acceleration_list.push_back(0);
+  }
+  minjerk.Reset(position_list, velocity_list, acceleration_list, time_list);
+  minjerk.StartInterpolation();
+
+  // Loop until end of trajectory
+  ros::Rate feedback_rate(100); // 10 msec
+  ros::Time now, prev = ros::Time::now();
+  do
+  {
     if (as_.isPreemptRequested())
     {
       ROS_INFO("%s: Preempted for %s gripper", node_name.c_str(), side_.c_str());
@@ -84,83 +114,36 @@ void GripperAction::executeCB(const control_msgs::FollowJointTrajectoryGoalConst
       return;
     }
 
-    float rad = goal->trajectory.points[i].positions[0];
+    feedback_rate.sleep();
+
+    ros::Time now = ros::Time::now();
     std_msgs::Float32 angle_msg;
+    double rad = minjerk.PassTime((now-prev).toSec());;
+    angle_msg.data = rad;
+    gripper_servo_angle_pub_.publish(angle_msg);
 
-    // Publish feedbacks until next point
-    ros::Rate feedback_rate(100); // 10 msec
-    ros::Time now;
-    //
-    float velocity = 0;
-    if ( i < goal->trajectory.points.size() - 1) {
-      float d0 = rad - prev_rad;
-      float d1 = goal->trajectory.points[i+1].positions[0];
-      float t0 = segment_time.toSec();
-      float t1 = goal->trajectory.points[i+1].time_from_start.toSec();
-      float v0 = d0/t0;
-      float v1 = d1/t1;
-      if ( v0 * v1 >= 0 ) {
-        velocity = 0.5 * (v0 + v1);
-      } else {
-        velocity = 0;
-      }
-    }
-    float x = prev_rad;
-    float v = 0;
-    float a = 0;
+    control_msgs::FollowJointTrajectoryFeedback feedback_;
 
-    float gx = rad;
-    float gv = velocity;
-    float ga = 0;
-    float target_t = segment_time.toSec();
-    float A=(gx-(x+v*target_t+(a/2.0)*target_t*target_t))/(target_t*target_t*target_t);
-    float B=(gv-(v+a*target_t))/(target_t*target_t);
-    float C=(ga-a)/target_t;
+    feedback_.joint_names.push_back(goal->trajectory.joint_names[0]);
+    feedback_.desired.positions.resize(1);
+    feedback_.actual.positions.resize(1);
+    feedback_.error.positions.resize(1);
 
-    float a0=x;
-    float a1=v;
-    float a2=a/2.0;
-    float a3=10*A-4*B+0.5*C;
-    float a4=(-15*A+7*B-C)/target_t;
-    float a5=(6*A-3*B+0.5*C)/(target_t*target_t);
-    do
-    {
-      // see seqplay::playPattern in https://github.com/fkanehiro/hrpsys-base/blob/master/rtc/SequencePlayer/seqplay.cpp
-      now = ros::Time::now();
-      // hoff arbib code is copied from https://github.com/fkanehiro/hrpsys-base/blob/master/rtc/SequencePlayer/interpolator.cpp
-      float t = (now - start_segment_time).toSec();
-      // angle_msg.data = (rad - prev_rad) * (now - start_segment_time).toSec()/segment_time.toSec() + prev_rad; // linear
-      angle_msg.data = a0+a1*t+a2*t*t+a3*t*t*t+a4*t*t*t*t+a5*t*t*t*t*t;
+    feedback_.desired.positions[0] = rad;
+    feedback_.actual.positions[0] = rad;
+    feedback_.error.positions[0] = 0;
 
-      gripper_servo_angle_pub_.publish(angle_msg);
-      feedback_rate.sleep();
+    feedback_.desired.time_from_start = now - start_time;
+    feedback_.actual.time_from_start = now - start_time;
+    feedback_.error.time_from_start = now - start_time;
+    feedback_.header.stamp = now;
+    as_.publishFeedback(feedback_);
 
-      control_msgs::FollowJointTrajectoryFeedback feedback_;
+    prev = now;
 
-      feedback_.joint_names.push_back(goal->trajectory.joint_names[0]);
-      feedback_.desired.positions.resize(1);
-      feedback_.actual.positions.resize(1);
-      feedback_.error.positions.resize(1);
+  } while (ros::ok() &&
+           now < (start_time + goal->trajectory.points[goal->trajectory.points.size()-1].time_from_start));
 
-      feedback_.desired.positions[0] = rad;
-      feedback_.actual.positions[0] = rad;
-      feedback_.error.positions[0] = 0;
-
-      feedback_.desired.time_from_start = now - start_time;
-      feedback_.actual.time_from_start = now - start_time;
-      feedback_.error.time_from_start = now - start_time;
-      feedback_.header.stamp = ros::Time::now();
-      as_.publishFeedback(feedback_);
-
-    } while (ros::ok() &&
-             now < (start_time + goal->trajectory.points[i].time_from_start));
-
-    start_segment_time = now;
-    prev_rad = rad;
-    x = gx;
-    v = gv;
-    a = ga;
-  }
   angle_state_ = prev_rad; // just in case anlge_stae is not updated;
 
   control_msgs::FollowJointTrajectoryResult result_;
