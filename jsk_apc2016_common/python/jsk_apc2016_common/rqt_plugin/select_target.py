a7aa60f98639ee59d72bcedc4893d7beeb08d784
Shingo Kitagawa <shingogo@hotmail.co.jp>
2016-10-16 17:51:14 +0900

    reinforce rqt_select_target to show target image

--- jsk_apc2016_common/python/jsk_apc2016_common/rqt_plugin/select_target.py
+++ jsk_apc2016_common/python/jsk_apc2016_common/rqt_plugin/select_target.py
@@ -6,7 +6,7 @@ import rospkg
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtGui import QDialog
+from python_qt_binding.QtGui import QDialog, QPixmap
 
 
 class SelectTargetWidget(QDialog):
@@ -19,7 +19,10 @@ class SelectTargetWidget(QDialog):
                 'select_target.ui'
                 )
         loadUi(ui_file, self)
-
+        self.image_path = os.path.join(
+                rp.get_path('jsk_apc2016_common'),
+                'models',
+                )
         work_order_list = rospy.get_param("/work_order")
         self.work_order = {}
         for order in work_order_list:
@@ -27,36 +30,37 @@ class SelectTargetWidget(QDialog):
         self.init_work_order = self.work_order
         self.bin_contents = rospy.get_param('/bin_contents')
         self.bin_dict = {
-                'bin_A': self.bin_A,
-                'bin_B': self.bin_B,
-                'bin_C': self.bin_C,
-                'bin_D': self.bin_D,
-                'bin_E': self.bin_E,
-                'bin_F': self.bin_F,
-                'bin_G': self.bin_G,
-                'bin_H': self.bin_H,
-                'bin_I': self.bin_I,
-                'bin_J': self.bin_J,
-                'bin_K': self.bin_K,
-                'bin_L': self.bin_L
+                'bin_A': {'combo': self.bin_A, 'image': self.bin_A_image},
+                'bin_B': {'combo': self.bin_B, 'image': self.bin_B_image},
+                'bin_C': {'combo': self.bin_C, 'image': self.bin_C_image},
+                'bin_D': {'combo': self.bin_D, 'image': self.bin_D_image},
+                'bin_E': {'combo': self.bin_E, 'image': self.bin_E_image},
+                'bin_F': {'combo': self.bin_F, 'image': self.bin_F_image},
+                'bin_G': {'combo': self.bin_G, 'image': self.bin_G_image},
+                'bin_H': {'combo': self.bin_H, 'image': self.bin_H_image},
+                'bin_I': {'combo': self.bin_I, 'image': self.bin_I_image},
+                'bin_J': {'combo': self.bin_J, 'image': self.bin_J_image},
+                'bin_K': {'combo': self.bin_K, 'image': self.bin_K_image},
+                'bin_L': {'combo': self.bin_L, 'image': self.bin_L_image}
                 }
 
         self.setObjectName('SelectTargetUI')
 
         for bin_ in 'abcdefghijkl':
             bin_ = 'bin_' + bin_.upper()
-            self.bin_dict[bin_].addItems(self.bin_contents[bin_])
-            self.bin_dict[bin_].currentIndexChanged.connect(
+            self.bin_dict[bin_]['combo'].addItems(self.bin_contents[bin_])
+            self.bin_dict[bin_]['combo'].currentIndexChanged.connect(
                     self._select_target(bin_)
                     )
         self.update.accepted.connect(self._update_param)
         self.update.rejected.connect(self._reset_param)
         self._get_init_index()
-        self._set_init_index()
+        self._set_init_target()
 
     def _select_target(self, bin_):
         def _select_target_curried(index):
             self.work_order[bin_] = self.bin_contents[bin_][index]
+            self._show_image(bin_, index)
         return _select_target_curried
 
     def _get_init_index(self):
@@ -67,10 +71,22 @@ class SelectTargetWidget(QDialog):
                     self.init_work_order[bin_]
                     )
 
-    def _set_init_index(self):
+    def _set_init_target(self):
         for bin_ in 'abcdefghijkl':
             bin_ = 'bin_' + bin_.upper()
-            self.bin_dict[bin_].setCurrentIndex(self.init_index[bin_])
+            self.bin_dict[bin_]['combo'].setCurrentIndex(self.init_index[bin_])
+            self._show_image(bin_, self.init_index[bin_])
+
+    def _show_image(self, bin_, index):
+        target_name = self.bin_contents[bin_][index]
+        target_image_path = os.path.join(
+                self.image_path,
+                target_name,
+                'image.jpg'
+                )
+        target_pixmap = QPixmap(target_image_path).scaled(100, 100)
+        self.bin_dict[bin_]['image'].setPixmap(target_pixmap)
+        self.bin_dict[bin_]['image'].show()
 
     def _update_param(self):
         self._set_param(self.work_order)
@@ -78,7 +94,7 @@ class SelectTargetWidget(QDialog):
 
     def _reset_param(self):
         self._set_param(self.init_work_order)
-        self._set_init_index()
+        self._set_init_target()
         self.show()
 
     def _set_param(self, work_order):
