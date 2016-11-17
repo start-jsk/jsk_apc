eb2b1e926312ff9fc88d4ddd2d49436411492e04
pazeshun <pazeshun3684@gmail.com>
2016-11-17 18:26:36 +0900

    Generalize prismatic_joint_controller.py

--- jsk_2016_01_baxter_apc/src/jsk_baxter_dxl_controllers/calib_required_joint_controller.py
+++ jsk_2016_01_baxter_apc/src/jsk_baxter_dxl_controllers/calib_required_joint_controller.py
@@ -3,7 +3,7 @@ import math
 import rospy
 from dynamixel_controllers.joint_position_controller import JointPositionController
 
-class PrismaticJointController(JointPositionController):
+class CalibRequiredJointController(JointPositionController):
     def __init__(self, dxl_io, controller_namespace, port_namespace):
         JointPositionController.__init__(self, dxl_io, controller_namespace, port_namespace)
 
@@ -15,6 +15,7 @@ class PrismaticJointController(JointPositionController):
 
         # Initialize joint position
         self.set_speed(0.0)
+        prev_limits = self.__get_angle_limits()       # Backup current angle limits
         self.__set_angle_limits(0, 0)                 # Change to wheel mode
         self.__set_speed_wheel(self.joint_speed if self.flipped else -self.joint_speed)
         rate = rospy.Rate(10)
@@ -23,7 +24,7 @@ class PrismaticJointController(JointPositionController):
                 break
             rate.sleep()
 
-        self.__set_angle_limits(4095, 4095)           # Change to multi-turn mode
+        self.__set_angle_limits(prev_limits['min'], prev_limits['max'])  # Change to previous mode
         self.set_speed(self.joint_speed)
         rospy.sleep(1.0)
 
@@ -46,6 +47,9 @@ class PrismaticJointController(JointPositionController):
 
     def __set_angle_limits(self, min_angle, max_angle):
         self.dxl_io.set_angle_limits(self.motor_id, min_angle, max_angle)
+
+    def __get_angle_limits(self):
+        return self.dxl_io.get_angle_limits(self.motor_id)
 
     def __get_feedback(self):
         return self.dxl_io.get_feedback(self.motor_id)
