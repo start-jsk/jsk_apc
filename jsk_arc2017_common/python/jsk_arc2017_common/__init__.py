902861e9a259a2c3c4a05ea5287dd026304767f9
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-06-07 02:36:38 +0900

    Fix typo

--- jsk_arc2017_common/python/jsk_arc2017_common/__init__.py
+++ jsk_arc2017_common/python/jsk_arc2017_common/__init__.py
@@ -4,7 +4,7 @@ import rospkg
 import yaml
 
 
-def get_object_names(id=False):
+def get_object_names():
     PKG_DIR = rospkg.RosPack().get_path('jsk_arc2017_common')
 
     with open(osp.join(PKG_DIR, 'config/label_names.yaml')) as f:
