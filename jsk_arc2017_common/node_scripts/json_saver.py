361e7d8b7c74c4cdf20cea23b49cc85ee4de4659
Shingo Kitagawa <shingogo@hotmail.co.jp>
2017-06-28 06:26:07 +0900

    update UpdateJSON and replace SaveJSON by Trigger

--- jsk_arc2017_common/node_scripts/json_saver.py
+++ jsk_arc2017_common/node_scripts/json_saver.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from jsk_arc2017_common.srv import SaveJSON
-from jsk_arc2017_common.srv import SaveJSONResponse
+import datetime
+
 from jsk_arc2017_common.srv import UpdateJSON
 from jsk_arc2017_common.srv import UpdateJSONResponse
 import json
 import os
 import os.path as osp
 import rospy
+from std_srvs.srv import Trigger
+from std_srvs.srv import TriggerResponse
 import threading
 
 
@@ -24,10 +26,13 @@ class JSONSaver(threading.Thread):
         if output_dir is None:
             rospy.logerr('must set output dir path to ~output_dir')
             return
+        now = datetime.datetime.now()
+        output_dir = osp.join(output_dir, now.strftime('%Y%m%d_%H%M%S'))
         if not osp.exists(output_dir):
-            os.mkdir(output_dir)
+            os.makedirs(output_dir)
         location_path = osp.join(json_dir, 'item_location_file.json')
-        self.output_json_path = osp.join(output_dir, 'item_location_file.json')
+        self.output_json_path = osp.join(
+            output_dir, 'item_location_file.json')
         with open(location_path) as location_f:
             data = json.load(location_f)
         self.bin_contents = {}
@@ -43,7 +48,7 @@ class JSONSaver(threading.Thread):
         self.services.append(rospy.Service(
             '~update_json', UpdateJSON, self._update))
         self.services.append(rospy.Service(
-            '~save_json', SaveJSON, self._save))
+            '~save_json', Trigger, self._save))
 
     def _update(self, req):
         is_updated = self._update_location(req)
@@ -53,7 +58,7 @@ class JSONSaver(threading.Thread):
 
     def _save(self, req):
         is_saved = self._save_json()
-        return SaveJSONResponse(saved=is_saved)
+        return TriggerResponse(success=is_saved)
 
     def _save_json(self):
         separators = (',', ': ')
@@ -93,15 +98,24 @@ class JSONSaver(threading.Thread):
 
     def _update_location(self, req):
         item = req.item
-        bin_ = req.bin
+        src = req.src
+        dst = req.dst
         is_updated = True
         self.lock.acquire()
-        try:
-            self.tote_contents.remove(item)
-            self.bin_contents[bin_].append(item)
-        except Exception:
-            rospy.logerr('{} does not exist in tote'.format(item))
-            is_updated = False
+        if src in 'ABC':
+            try:
+                self.bin_contents[src].remove(item)
+                self.bin_contents[dst].append(item)
+            except Exception:
+                rospy.logerr('{0} does not exist in bin {1}'.format(item, src))
+                is_updated = False
+        else:
+            try:
+                self.tote_contents.remove(item)
+                self.bin_contents[dst].append(item)
+            except Exception:
+                rospy.logerr('{} does not exist in tote'.format(item))
+                is_updated = False
         self.lock.release()
         return is_updated
 
