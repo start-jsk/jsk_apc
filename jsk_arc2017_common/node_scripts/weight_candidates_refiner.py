7d126aeea374823aa00cdb4970f5cfc5c49b15d9
Shingo Kitagawa <shingogo@hotmail.co.jp>
2017-07-11 03:14:14 +0900

    refine weight_candidates_refiner node

--- jsk_arc2017_common/node_scripts/weight_candidates_refiner.py
+++ jsk_arc2017_common/node_scripts/weight_candidates_refiner.py
@@ -14,20 +14,23 @@ class WeightCanditatesRefiner(ConnectionBasedTransport):
 
     def __init__(self):
         super(WeightCanditatesRefiner, self).__init__()
-        self.scale_inputs = rospy.get_param('~scale_inputs')
+        # {object_name: object_id}
+        self.candidates = {}
+        self.input_topics = rospy.get_param('~input_topics')
+        # {object_name: weight}
         self.object_weights = rospy.get_param('~object_weights')
         self.error = rospy.get_param('~error', 1.0)
 
-        self.scale_values = [0.0] * len(self.scale_inputs)
-        self.init_sum = 0.0
+        self.weight_sum = 0.0
+        self.prev_weight_values = [0] * len(self.input_topics)
+
         self.weight_sum_pub = self.advertise(
             '~output/weight_sum', WeightStamped, queue_size=1)
         self.picked_pub = self.advertise(
             '~output/candidates/picked', LabelArray, queue_size=1)
         self.placed_pub = self.advertise(
             '~output/candidates/placed', LabelArray, queue_size=1)
-        self.init_srv = rospy.Service(
-            '~initialize', Trigger, self._initialize)
+        self.init_srv = rospy.Service('~reset', Trigger, self._reset)
 
     def subscribe(self):
         use_async = rospy.get_param('~approximate_sync', False)
@@ -37,8 +40,8 @@ class WeightCanditatesRefiner(ConnectionBasedTransport):
             '~input/candidates', LabelArray, self._candidates_cb)
         # add scale subscriber
         self.subs = []
-        for scale_input in self.scale_inputs:
-            sub = message_filters.Subscriber(scale_input, WeightStamped)
+        for input_topic in self.input_topics:
+            sub = message_filters.Subscriber(input_topic, WeightStamped)
             self.subs.append(sub)
         if use_async:
             slop = rospy.get_param('~slop', 0.1)
@@ -55,55 +58,61 @@ class WeightCanditatesRefiner(ConnectionBasedTransport):
             sub.unregister()
 
     def _candidates_cb(self, labels_msg):
-        candidates = {}
+        self.candidates = {}
         for label_msg in labels_msg.labels:
-            candidates[label_msg.name] = label_msg.id
+            self.candidates[label_msg.name] = label_msg.id
 
-        weight_sum = sum(self.scale_values)
-        weight_diff = weight_sum - self.init_sum
+    def _scale_cb(self, *weight_msgs):
+        if not self.candidates:
+            rospy.logwarn_throttle(10, 'No candidates, so skipping')
+            return
+        candidates = self.candidates
+
+        assert len(weight_msgs) == len(self.prev_weight_values)
+        weight_values = [w.weight.value for w in weight_msgs]
+        if any(w < 0 for w in weight_values):
+            return  # unstable, scale over, or something
+        self.prev_weight_values = weight_values
+
+        weight_sum = sum(weight_values)
+        weight_diff = weight_sum - self.weight_sum
         sum_msg = WeightStamped()
-        sum_msg.header = labels_msg.header
-        sum_msg.weight.value = weight_sum
-        sum_msg = WeightStamped()
-        sum_msg.header = labels_msg.header
+        sum_msg.header = weight_msgs[0].header
         sum_msg.weight.value = weight_sum
 
         pick_msg = LabelArray()
         place_msg = LabelArray()
-        pick_msg.header = labels_msg.header
-        place_msg.header = labels_msg.header
+        pick_msg.header = weight_msgs[0].header
+        place_msg.header = weight_msgs[0].header
         diff_lower = weight_diff - self.error
         diff_upper = weight_diff + self.error
-        for obj, w in self.object_weights.items():
-            if obj not in candidates.keys():
+        for obj_name, w in self.object_weights.items():
+            if obj_name not in candidates:
                 continue
+            obj_id = candidates[obj_name]
             if diff_upper > w and w > diff_lower:
                 label = Label()
-                label.id = candidates[obj]
-                label.name = obj
+                label.id = obj_id
+                label.name = obj_name
                 place_msg.labels.append(label)
             elif diff_upper > -w and -w > diff_lower:
                 label = Label()
-                label.id = candidates[obj]
-                label.name = obj
+                label.id = obj_id
+                label.name = obj_name
                 pick_msg.labels.append(label)
 
         self.weight_sum_pub.publish(sum_msg)
         self.picked_pub.publish(pick_msg)
         self.placed_pub.publish(place_msg)
 
-    def _scale_cb(self, *args):
-        assert len(args) == len(self.scale_values)
-        for i, weight_msg in enumerate(args):
-            self.scale_values[i] = weight_msg.weight.value
-
-    def _initialize(self, req):
+    def _reset(self, req):
         is_success = True
         try:
-            self.init_sum = sum(self.scale_values)
+            self.weight_sum = sum(self.prev_weight_values)
         except Exception:
             is_success = False
         return TriggerResponse(success=is_success)
+
 
 if __name__ == '__main__':
     rospy.init_node('weight_candidates_refiner')
