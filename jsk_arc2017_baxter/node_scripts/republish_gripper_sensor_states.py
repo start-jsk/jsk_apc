19a48d90a9229af42850acb250598db5a476f46d
Shun Hasegawa <pazeshun3684@gmail.com>
2017-07-12 09:05:01 +0900

    Fix topic of republish_gripper_sensor_states.py

--- jsk_arc2017_baxter/node_scripts/republish_gripper_sensor_states.py
+++ jsk_arc2017_baxter/node_scripts/republish_gripper_sensor_states.py
@@ -15,16 +15,16 @@ class RepublishGripperSensorStates(ConnectionBasedTransport):
     def __init__(self):
         super(RepublishGripperSensorStates, self).__init__()
         self.pub_prox = self.advertise(
-            'proximity_array', ProximityArray,
+            '~proximity_array', ProximityArray,
             queue_size=1)
         self.pub_pressure = self.advertise(
-            'pressure/state', Float64,
+            '~pressure/state', Float64,
             queue_size=1)
         self.pub_r_finger_flex = self.advertise(
-            'flex/right/state', UInt16,
+            '~flex/right/state', UInt16,
             queue_size=1)
         self.pub_l_finger_flex = self.advertise(
-            'flex/left/state', UInt16,
+            '~flex/left/state', UInt16,
             queue_size=1)
         # low-pass filtered proximity reading
         self.average_value = []
