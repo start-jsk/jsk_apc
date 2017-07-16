27ef2607d68d8655d04b01c8fd818f11079b5a50
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-16 15:59:56 +0900

    Disable of downloading old chainer models

--- jsk_arc2017_common/scripts/install_data.py
+++ jsk_arc2017_common/scripts/install_data.py
@@ -22,21 +22,21 @@ def download_data(path, url, md5):
 
 
 def main():
-    # dataset: v1
-    # augmentation: standard
-    download_data(
-        path='data/models/fcn32s_arc2017_dataset_v1_20170326_005.pth',
-        url='https://drive.google.com/uc?id=0B9P1L--7Wd2vT1pnWnVsNERHTVk',
-        md5='ae9d13c126389bd63bccf0db1551f31e',
-    )
+    # # dataset: v1
+    # # augmentation: standard
+    # download_data(
+    #     path='data/models/fcn32s_arc2017_dataset_v1_20170326_005.pth',
+    #     url='https://drive.google.com/uc?id=0B9P1L--7Wd2vT1pnWnVsNERHTVk',
+    #     md5='ae9d13c126389bd63bccf0db1551f31e',
+    # )
 
-    # dataset: v1
-    # augmentation: stack
-    download_data(
-        path='data/models/fcn32s_arc2017_dataset_v1_20170417.pth',
-        url='https://drive.google.com/uc?id=0B9P1L--7Wd2vYWloN0FGeEhlcGs',
-        md5='a098399a456de29ef8d4feaa8ae795e9',
-    )
+    # # dataset: v1
+    # # augmentation: stack
+    # download_data(
+    #     path='data/models/fcn32s_arc2017_dataset_v1_20170417.pth',
+    #     url='https://drive.google.com/uc?id=0B9P1L--7Wd2vYWloN0FGeEhlcGs',
+    #     md5='a098399a456de29ef8d4feaa8ae795e9',
+    # )
 
     # dataset: v2
     # augmentation: stack
