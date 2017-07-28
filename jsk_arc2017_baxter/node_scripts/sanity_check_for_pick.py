52088dfc7726d111682fd21235c5c3dfe80e6af9
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-29 08:37:38 +0900

    Check sanity of system (topics/nodes) by script

---
+++ jsk_arc2017_baxter/node_scripts/sanity_check_for_pick.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os.path as osp
+
+import rospy
+import sensor_msgs.msg
+
+from jsk_tools.sanity_lib import checkNodeState
+from jsk_tools.sanity_lib import checkTopicIsPublished
+
+
+def main():
+    # check node is exists
+    nodes = [
+    ]
+    for node in nodes:
+        checkNodeState(node, needed=True)
+
+    # common for left/right hand camera
+    topics = [
+        '/json_saver/output/bin_contents',
+        '/json_saver/output/json_dir',
+    ]
+    for topic in topics:
+        if not checkTopicIsPublished(topic, timeout=5):
+            return
+
+    # for left/right hand camera
+    topics = [
+        # pick.launch
+        'candidates_publisher/output/candidates',
+    ]
+    for side in ['left', 'right']:
+        for topic in topics:
+            topic = '/%s_hand_camera/%s' % (side, topic)
+            if not checkTopicIsPublished(topic, timeout=30):
+                return
+
+
+if __name__ == '__main__':
+    rospy.init_node('sanity_check_for_pick')
+    main()
