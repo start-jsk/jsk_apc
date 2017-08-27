3dbf4deb29f5c0439d2779115923f109b75209e0
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-27 23:24:42 +0900

    Remove no longer needed build.sh (moved to hub.docker.io)
    http://hub.docker.com/r/wkentaro/jsk_apc

--- docker/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-sudo docker build -t jsk-apc-indigo .
-(echo -e "FROM jsk-apc-indigo\nRUN apt-get update\nRUN apt-get -y upgrade\nEXPOSE 22" | sudo docker build -t jsk-apc-indigo -)
