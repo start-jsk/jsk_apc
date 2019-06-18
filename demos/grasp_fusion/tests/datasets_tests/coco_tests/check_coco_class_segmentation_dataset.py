fa45796e4647ff46276e15181ddc52d9689d5741
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-18 16:08:00 +0100

    Remove pycocotools from dependency

--- demos/grasp_fusion/tests/datasets_tests/coco_tests/check_coco_class_segmentation_dataset.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    import grasp_fusion_lib
-    dataset = grasp_fusion_lib.datasets.coco.COCOClassSegmentationDataset(
-        'minival')
-    grasp_fusion_lib.datasets.view_class_seg_dataset(dataset)
