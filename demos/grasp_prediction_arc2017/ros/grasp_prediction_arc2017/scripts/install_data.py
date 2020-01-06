1491f4dc406075e32711eceb9776baec4aa84f87
Shun Hasegawa <pazeshun3684@gmail.com>
2020-01-06 12:12:52 +0900

    [grasp_prediction_arc2017] Download other large data required for hasegawa iros2018 demo

--- demos/grasp_prediction_arc2017/ros/grasp_prediction_arc2017/scripts/install_data.py
+++ demos/grasp_prediction_arc2017/ros/grasp_prediction_arc2017/scripts/install_data.py
@@ -23,6 +23,23 @@ def main():
         md5='d2ace2f1b45f862b8976f81637a8e3a7',
     )
 
+    path = osp.expanduser('~/data/hasegawa_iros2018/system_inputs/ForItemDataBooks6/FCN8sAtOnce_180720_130702_iter00060000.npz')  # NOQA
+    jsk_data.download_data(
+        pkg_name='grasp_prediction_arc2017',
+        path=path,
+        url='https://drive.google.com/uc?id=1ADYXvEouFL3ynnb8xBCmcAzgD8J-ddpm',
+        md5='09fc1db2f5ff1a3f04366024e5921706',
+    )
+
+    path = osp.expanduser('~/data/hasegawa_iros2018/system_inputs/ForItemDataBooks6/objects.zip')  # NOQA
+    jsk_data.download_data(
+        pkg_name='grasp_prediction_arc2017',
+        path=path,
+        url='https://drive.google.com/uc?id=1dvC_QRtathHrg_PseCNkSiXwWmiT9IB_',
+        md5='d25bff88acc16313210237da4f6367c4',
+        extract=True,
+    )
+
 
 if __name__ == '__main__':
     main()
